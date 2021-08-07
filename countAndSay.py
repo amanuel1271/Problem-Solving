45eb15cffeaea9d23d0cde84095630b5b215832c
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-07 12:30:16 +0900

    Create countAndSay.py

---
+++ countAndSay.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        
+        prev = self.countAndSay(n-1)
+        i = 0
+        returnstr = ''
+        
+        while i < len(prev):
+            digit = prev[i]
+            count = 1
+            
+            i += 1
+            
+            while i < len(prev) and prev[i] == digit:
+                count += 1
+                i += 1
+                
+            returnstr += str(count) + digit
+                
+                
+            
+        return returnstr
+            
+            
