6eddd129cf7231a1266b2fa42965a8bbd8062912
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-08 03:02:09 +0900

    Update countAndSay.cpp

--- countAndSay.cpp
+++ countAndSay.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if (n == 1){
-            string returnstr = string("1");
-            return returnstr;
-        }
+        if (n == 1)
+            return string("1");
+        
                 
         string prev = countAndSay(n - 1);
         
         int i = 0;
-        string returnstr = "";
+        string returnstr = string("");
         int len_ = prev.size();
         
         while (i < len_){
