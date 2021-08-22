b1b824b0421fd1a75f56d1273d9e50945dbf4117
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-23 06:16:52 +0900

    Update countUniqueDigits.py

--- countUniqueDigits.py
+++ countUniqueDigits.py
@@ -10,6 +10,13 @@ class Solution:
         for j in range(n):
             count += self.store[j] 
         return count
+    
+    def count_occur(self,n):
+        count = 9
+        for i in range(n - 2):
+            count = count * (9 - i)  
+        count = count * (n - 1)
+        return count
          
         
     def find_dup(self,n):
@@ -23,18 +30,12 @@ class Solution:
             
         prev_dup -= prev_count
         
-        mult_by_10 = prev_dup * 10 
-        count = 9
-        for i in range(n - 2):
-            count = count * (9 - i)  
-        count = count * (n - 1)
-        self.store[n] = mult_by_10 + count
+        cur_dup = prev_dup * 10 
+        self.store[n] = cur_dup + self.count_occur(n)
         
-        count_2 = 0
-        for j in range(n):
-            count_2 += self.store[j]
+        count_2 = self.prev_sum(n)
           
-        return mult_by_10 + count + count_2
+        return self.store[n] + count_2
     
     def countNumbersWithUniqueDigits(self, n: int) -> int:
         return (10 ** n) - self.find_dup(n)
