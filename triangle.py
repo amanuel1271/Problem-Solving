26bada4cecbbb530559e2d0c74fea08592d4e288
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-25 06:28:52 +0900

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,12 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        height = len(triangle)
-        dp = [[0]*(i + 1) for i in range(height)]
-        
-        dp[-1] = triangle[-1]
-        
-        for i in range(height - 1):
+        for i in range(len(triangle) - 1):
             for j in range(len(triangle[-2-i])):
-                dp[-2-i][j] = min(triangle[-2-i][j] + dp[-1-i][j],triangle[-2-i][j] + dp[-1-i][j+1])
+                triangle[-2-i][j] = min(triangle[-2-i][j] + triangle[-1-i][j],triangle[-2-i][j] + triangle[-1-i][j+1]) 
                 
-        return min(dp[0])
+        return min(triangle[0])
