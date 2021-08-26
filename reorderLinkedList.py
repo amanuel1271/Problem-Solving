3dde2ad09e0ba55efe5fd0992e31a37365c66baf
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-27 04:44:55 +0900

    Update reorderLinkedList.py

--- reorderLinkedList.py
+++ reorderLinkedList.py
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    void reorderList(ListNode* head) {
+    void unlink(ListNode *next_node,ListNode *find_next){
+        ListNode *unlink = next_node;
+        
+        while (unlink && unlink->next != find_next)
+            unlink = unlink->next;
+        
+        if (unlink)
+            unlink->next = NULL;    
+    }
+    
+    void reorderList(ListNode *head) {
         if (head == NULL)
             return;
             
         ListNode *find_next = head;
         ListNode *next_node = head->next;
         
-        if (next_node == NULL)
-            return;
+        if (next_node == NULL)  return;
         
         
-        while (find_next->next)
-            find_next = find_next->next;
+        while (find_next->next)  find_next = find_next->next;
         
         if (next_node != find_next){
             head->next = find_next;
             find_next->next = next_node;
         }
-        
-        ListNode *unlink = next_node;
-        while (unlink && unlink->next != find_next)
-            unlink = unlink->next;
-        
-        if (unlink)
-            unlink->next = NULL;
-        
+      
+        unlink(next_node,find_next);
         reorderList(next_node);
         
         
