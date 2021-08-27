02377029fc7cf10329b36d702c0bf43f98995188
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-27 18:17:23 +0900

    Create decodeString.py

---
+++ decodeString.py
@@ -0,0 +1,40 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        i = 0
+        final_string = ''
+
+        
+        while i < len(s):
+        
+            while i < len(s) and not s[i].isdigit():
+                final_string += s[i]
+                i += 1
+
+            if i >= len(s):
+                break
+                
+            num_st = ''
+            
+            while s[i] != "[":
+                num_st += s[i]
+                i += 1
+                
+            recurse_string =  ''
+            num = int(num_st)
+            i += 1
+            list_ = []
+
+
+            while i < len(s) and (s[i] != ']' or list_):
+                recurse_string += s[i]
+                if s[i] == "[":
+                    list_.append('[')
+                if s[i] == ']' and list_:
+                    list_.pop(0)
+                i += 1
+    
+
+            final_string += num * self.decodeString(recurse_string)
+            i += 1
+        
+        return final_string
