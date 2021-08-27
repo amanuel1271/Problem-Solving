36df5d9ff7e41b78dba8330f8557fbad56519dad
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-27 18:34:55 +0900

    Delete triangle.py

--- triangle.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for i in range(len(triangle) - 1):
-            for j in range(len(triangle[-2-i])):
-                triangle[-2-i][j] = min(triangle[-2-i][j] + triangle[-1-i][j],triangle[-2-i][j] + triangle[-1-i][j+1]) 
-                
-        return min(triangle[0])
