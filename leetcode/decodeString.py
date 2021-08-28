810616eb69af0e00737ffa56f5829389be62b4f7
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-28 15:15:13 +0900

    Update decodeString.py

--- leetcode/decodeString.py
+++ leetcode/decodeString.py
@@ -10,22 +10,22 @@ class Solution:
             if i == len(s):
                 break
                 
-            num_st = ''
+            num_str = ''
             while s[i] != "[":
-                num_st += s[i]
+                num_str += s[i]
                 i += 1
                 
             recurse_string =  ''
-            num = int(num_st)
+            num = int(num_str)
             i += 1
-            list_ = []
+            bracket_list_ = []
             
-            while i < len(s) and (s[i] != ']' or list_):
+            while i < len(s) and (s[i] != ']' or bracket_list_):
                 recurse_string += s[i]
                 if s[i] == "[":
-                    list_.append('[')
+                    bracket_list_.append('[')
                 if s[i] == ']' and list_:
-                    list_.pop(0)
+                    bracket_list_.pop(0)
                 i += 1
     
             final_string += num * self.decodeString(recurse_string)
