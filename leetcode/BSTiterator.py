daa29daac1b558c984c77b71f1c053f15517f2e0
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-29 21:21:16 +0900

    Create BSTiterator.py

---
+++ leetcode/BSTiterator.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.iterator = []
+        self.inorder(root)
+        self.iterator = [self.iterator[0] - 1] + self.iterator
+        self.index = 0
+    def inorder(self,root):
+        if not root:
+            return 
+        
+        self.inorder(root.left)
+        self.iterator.append(root.val)
+        self.inorder(root.right)
+        
+    def next(self) -> int:
+        self.index += 1
+        return self.iterator[self.index]
+        
+
+    def hasNext(self) -> bool:
+        #print(self.index,self.iterator)
+        return self.index < len(self.iterator) - 1
+    
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
