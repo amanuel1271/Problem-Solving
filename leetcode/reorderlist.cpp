1e5940808699838912d3d554ea740a16198c16ee
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-08-30 00:05:22 +0900

    Update reorderlist.cpp

--- leetcode/reorderlist.cpp
+++ leetcode/reorderlist.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     void unlink(ListNode *next_node,ListNode *find_next){
         ListNode *unlink = next_node;
-        
         while (unlink && unlink->next != find_next)
             unlink = unlink->next;
-        
         if (unlink)
             unlink->next = NULL;    
     }
@@ -13,23 +11,18 @@ public:
     void reorderList(ListNode *head) {
         if (head == NULL)
             return;
-            
+        
         ListNode *find_next = head;
         ListNode *next_node = head->next;
         
         if (next_node == NULL)  return;
-        
-        
         while (find_next->next)  find_next = find_next->next;
         
         if (next_node != find_next){
             head->next = find_next;
             find_next->next = next_node;
         }
-      
         unlink(next_node,find_next);
-        reorderList(next_node);
-        
-        
+        reorderList(next_node);  
     }
 };
