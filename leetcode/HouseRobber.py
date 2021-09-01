e3d4b736c567ebb23fad93095e0ea62356db62f9
amanuel1271 <50099232+amanuel1271@users.noreply.github.com>
2021-09-01 22:28:04 +0900

    Update HouseRobber.py

--- leetcode/HouseRobber.py
+++ leetcode/HouseRobber.py
@@ -3,10 +3,12 @@ class Solution:
         dp = [0] * len(nums)
         dp[0] = nums[0]
         
-        for j in range(1,len(nums)):
-            if j == 1:
-                dp[1] = max(nums[0],nums[1])
-                continue
+        if len(nums) == 1:
+            return dp[-1]
+        
+        dp[1] = max(nums[0],nums[1])
+        
+        for j in range(2,len(nums)):
             dp[j] = max(nums[j] + dp[j - 2],dp[j-1])
             
         return dp[-1]
