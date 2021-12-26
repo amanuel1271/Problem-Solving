5fb8078767707305e6859724a45535271fe1d154
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2021-12-27 05:10:11 +0900

    Create MaxConsecutiveOnes2.py

---
+++ leetcode/MaxConsecutiveOnes2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        maxx = 0
+        curr,prev,seenZero = 0,0,0
+        
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                seenZero = 1
+                prev = curr
+                curr = 0
+            else:
+                curr += 1
+            
+            maxx = max(maxx,curr + prev + seenZero)
+        return maxx
