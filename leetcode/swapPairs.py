f7b35be586537932949696dbff869084cfbe496d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2021-12-27 21:49:36 +0900

    Create swapPairs.py

---
+++ leetcode/swapPairs.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        def join(prev,curr):
+            if prev == None:
+                return curr
+            
+            attach = prev
+            
+            while attach.next:
+                attach = attach.next
+            attach.next = curr
+            
+            return prev
+        
+        def helper(prev,curr):
+            if curr == None:
+                return prev
+            
+            if curr.next == None:
+                return helper(join(prev,curr),None)
+            
+            nextt = curr.next.next
+            save = curr.next
+            curr.next = None
+            save.next = curr
+
+            return helper(join(prev,save),nextt)
+            
+        return helper(None,head)
