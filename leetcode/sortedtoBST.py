aecbeca10e72102a3aedc6ca26608866bbf4ff61
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-04 04:05:51 +0900

    Update sortedtoBST.py

--- leetcode/sortedtoBST.py
+++ leetcode/sortedtoBST.py
@@ -1,28 +1,21 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        
-        def helper(numlist):
-            
-            lenn = len(numlist)
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        def helper(sortedlst):
+            lenn = len(sortedlst)
             if lenn == 0:
                 return None
-            elif lenn == 1:
-                return TreeNode(numlist[0],None,None)
-            elif lenn == 2:
-                return TreeNode(numlist[1],TreeNode(numlist[0]),None)
             
-            mid = lenn // 2
-            
-            return TreeNode(numlist[mid],helper(numlist[:mid]),helper(numlist[mid+1:]))
+            mid = lenn//2
+            root = TreeNode(sortedlst[mid])
+            root.left = helper(sortedlst[:mid])
+            root.right = helper(sortedlst[mid + 1:])
+            return root
             
             
         
-        nums.sort()
-        return helper(nums)
-        
+        lst = []
+        while head:
+            lst.append(head.val)
+            head = head.next
+            
+        return helper(lst)
