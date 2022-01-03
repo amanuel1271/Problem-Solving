f6cdf599c5622c497b782170e671ece5f9d20041
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-04 08:04:30 +0900

    Update flattenBinTree.py

--- leetcode/flattenBinTree.py
+++ leetcode/flattenBinTree.py
@@ -1,39 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def rearrange(self,root):
-        if not root.left and not root.right:
-            return root,root # head and tail pointers
-        
-        if root.left:
-            left_head,left_tail = self.rearrange(root.left)
-            if root.right:
-                right_head,right_tail =  self.rearrange(root.right)
-                root.right = left_head
-                left_tail.right = right_head
-                root.left = None
-            else:
-                root.right = left_head
-                root.left = None
-                right_tail = left_tail
-                
-        else:
-            if root.right:
-                right_head,right_tail =  self.rearrange(root.right) 
-                
-        return root,right_tail
-            
     def flatten(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        if not root:
-            return
-        self.rearrange(root)
+        def helper(root):
+            if not root:
+                return root
+            elif not root.left and not root.right:
+                return root
+            
+
+            left,right = helper(root.left),helper(root.right)
+            if not left:
+                root.right = right
+                return root
+            
+            save_left = left
+            while save_left.right:
+                save_left = save_left.right
+                
+            save_left.right = right
+            root.right = left
+            root.left = None
+            return root
         
         
         
