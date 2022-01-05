c6ee82e499d434ea5107278b384f4d42f69f6e0c
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-06 06:15:45 +0900

    Update 108.py

--- leetcode/108.py
+++ leetcode/108.py
@@ -6,16 +6,11 @@ class Solution:
                 return None
             
             mid = lenn//2
-            root = TreeNode(sortedlst[mid])
-            root.left = helper(sortedlst[:mid])
-            root.right = helper(sortedlst[mid + 1:])
+            root = TreeNode(sortedlst[mid],helper(sortedlst[:mid]),helper(sortedlst[mid + 1:]))
             return root
             
-            
-        
         lst = []
         while head:
             lst.append(head.val)
             head = head.next
-            
         return helper(lst)
