6fada77b38f614993ec3301615651aed373d9476
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-08 07:12:51 +0900

    Update 200_goodQ.py

--- leetcode/200_goodQ.py
+++ leetcode/200_goodQ.py
@@ -1,12 +1,9 @@
 class Solution:
     def numIslands(self, grid):
-        s,num = set(),0
-        for i, row in enumerate(grid):
-            for j, col in enumerate(row):
-                if col == '1':
-                    s.add((i,j))
-            
-            
+#  using bfs and queue 
+        s = {(i,j) for i,row in enumerate(grid) for j, col in enumerate(row) if col == '1'}
+        num = 0
+               
         while s:
             num += 1
             queue = deque([s.pop()])
@@ -17,3 +14,30 @@ class Solution:
                         s.remove(item)
                         queue.append(item)
         return num
+
+class Solution:
+    def numIslands(self, grid):
+        #  attempt using stack and dfs
+        s = {(i,j) for i,row in enumerate(grid) for j, col in enumerate(row) if col == '1'}
+        num = 0 
+        
+        while s:
+            num += 1
+            x,y = s.pop()
+            stack = deque([(x,y)])
+            
+            while stack:
+                x,y = stack[-1]
+                if (x + 1,y) not in s and (x - 1,y) not in s and (x,y + 1) not in s and (x,y - 1) not in s:
+                    stack.pop()
+                for nei in [(x - 1,y),(x + 1,y), (x, y - 1), (x, y + 1)]:
+                    if nei in s:
+                        stack.append(nei)
+                        s.remove(nei)
+                        break
+                        
+        return num
+                
+                
+                    
+                
