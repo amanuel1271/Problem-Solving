6ad9f472d03b7dc5fb5adabe1b039aa9e863274b
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-07 18:03:04 +0900

    Update 70.py

--- leetcode/70.py
+++ leetcode/70.py
@@ -1,18 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def climbcache(n,cache):
-            
-            def getcache(i,cache):
-                if i in cache:
-                    return cache[i]
-                cache[i] = climbcache(i,cache)
-                return cache[i]
-                
-            if n in cache:
-                return cache[n]
-            
-            cache[n] = getcache(n-1,cache) + getcache(n-2,cache)
-            return cache[n]
-            
-            
-        return climbcache(n,{1:1,2:2})
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+              
+        return dp[-1]
