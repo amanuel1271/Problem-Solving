75ed2ca4cfd3b565f4eefb925563ab8e510ecf16
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-08 15:22:51 +0900

    Update 394.py

--- leetcode/394.py
+++ leetcode/394.py
@@ -1,35 +1,15 @@
 class Solution:
-    def decodeString(self, s: str) -> str:
-        i,final_string= 0,''
-        
-        len_ = len(s)
-        while i < len_:
-            while i < len_ and not s[i].isdigit():
-                final_string += s[i]
-                i += 1
-         
-            if i == len_:
-                break
-                
-            num_str = ''
-            while s[i] != "[":
-                num_str += s[i]
-                i += 1
-                
-            recurse_string =  ''
-            num = int(num_str)
-            i += 1
-            bracket_list_ = []
-            
-            while i < len_ and (s[i] != ']' or bracket_list_):
-                recurse_string += s[i]
-                if s[i] == "[":
-                    bracket_list_.append('[')
-                if s[i] == ']' and bracket_list_:
-                    bracket_list_.pop(0)
-                i += 1
-    
-            final_string += num * self.decodeString(recurse_string)
-            i += 1
-        
-        return final_string
+    def decodeString(self, s):
+        stk, ans, n = deque(), "", ""
+        for c in s:
+            if c.isalpha():   
+                ans += c
+            elif c.isdigit(): 
+                n += c
+            elif c == '[': 
+                stk.append((n, ans))
+                n, ans = "", ""
+            else:
+                cnt, prev = stk.pop()
+                ans = prev + ans * int(cnt)
+        return ans
