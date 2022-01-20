b504ef7d8b78c00fade725a0012b89bd3edc8439
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-21 01:35:07 +0900

    Create 207.py

---
+++ leetcode/207.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # use kahn topological sorting method
+        # if it is acyclic and all courses can be taken return True else False
+        
+        prereq,courses_that_need = defaultdict(int),defaultdict(list)
+        
+        for need,prerequisite in prerequisites:
+            prereq[need] += 1
+            courses_that_need[prerequisite].append(need)
+            
+            
+        Q = deque()
+        visited = set()
+        for course in range(numCourses):
+            if prereq[course] == 0:
+                Q.append(course)
+                visited.add(course)
+            
+        while Q:
+            course = Q.popleft()
+            for classes in courses_that_need[course]:
+                prereq[classes] -= 1
+                if prereq[classes] == 0:
+                    Q.append(classes)
+                    visited.add(classes)
+
+        
+        
+        return len(visited) == numCourses
