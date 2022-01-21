cd992fb5f0b0b5d735cc691ea70d3d411269773f
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-22 01:28:36 +0900

    Update 746.py

--- leetcode/746.py
+++ leetcode/746.py
@@ -4,9 +4,7 @@ class Solution:
         
         
         for i in range(2, len(cost) + 1):
-            take_one_step = minimum_cost[i - 1] + cost[i - 1]
-            take_two_steps = minimum_cost[i - 2] + cost[i - 2]
-            minimum_cost[i] = min(take_one_step, take_two_steps)
+            minimum_cost[i] = min(minimum_cost[i - 1] + cost[i - 1],minimum_cost[i - 2] + cost[i - 2])
 
 
         return minimum_cost[-1]
