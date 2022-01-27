29d56998829345bb620782d72d197c1f582d2fdb
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-28 05:48:32 +0900

    Update 1137.py

--- leetcode/1137.py
+++ leetcode/1137.py
@@ -5,11 +5,10 @@ class Solution:
         elif n <= 2:
             return 1
             
-        dp = [0] * (n + 1)
         
-        dp[0],dp[1],dp[2] = 0,1,1
+        back_back,back,front = 0,1,1
         
         for j in range(3,n + 1):
-            dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
+            back_back,back,front = back,front,front + back_back + back
         
-        return dp[n]
+        return front
