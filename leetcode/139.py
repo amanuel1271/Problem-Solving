5c86989935652430c231c9c2a00ffc4778e4a02a
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-01-29 05:50:26 +0900

    Create 139.py

---
+++ leetcode/139.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        preprocess = defaultdict(list)
+        for words in wordDict:
+            preprocess[words[0]].append(words)
+        
+
+        
+        @lru_cache(None)
+        def dp(word):
+            if word == '':
+                return True
+            
+            for words in preprocess[word[0]]:
+                cut_len = len(words)
+                if cut_len <= len(word) and word[:cut_len] == words:
+                    if dp(word[cut_len:]):
+                        return True
+            return False
+        
+        return dp(s)
