0a90fdffbfe5df6ad37ac063b940d5b5f3c88f06
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-01 21:41:51 +0900

    Update 91.py

--- leetcode/91.py
+++ leetcode/91.py
@@ -6,32 +6,21 @@ class Solution:
         
         @lru_cache(None)
         def dp(word):
-            lenn = len(word)
+            l = len(word)
             
-            if lenn == 1:
-                if word == '0':
-                    return 0
-                return 1
-            elif lenn == 2:
+            if l == 1:
+                return 0 if word == '0' else 1
+            elif l == 2:
                 if word in strnum_to_alpha:
-                    if word[0] in strnum_to_alpha and word[1] in strnum_to_alpha:
-                        return 2
-                    else:
-                        return 1
+                    return 2 if word[0] in strnum_to_alpha and word[1] in strnum_to_alpha else 1       
                 else:
-                    if word[0] in strnum_to_alpha and word[1] in strnum_to_alpha:
-                        return 1
-                    else:
-                        return 0
+                    return 1 if word[0] in strnum_to_alpha and word[1] in strnum_to_alpha else 0
             
             first,second = dp(word[0]),0 
             if (word[0:2] in strnum_to_alpha):
-                second = dp(word[2:])
-                
+                second = dp(word[2:])  
             if first == 0:
                 return second
-                
-            
             return dp(word[1:]) + second
         
         return dp(s)
