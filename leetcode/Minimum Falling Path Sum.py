f7932598b510a1a27f6c6da64fe8f958f091a549
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-04 20:27:59 +0900

    Update Minimum Falling Path Sum.py

--- leetcode/Minimum Falling Path Sum.py
+++ leetcode/Minimum Falling Path Sum.py
@@ -5,16 +5,8 @@ class Solution:
         for row in range(1,n):
             for col in range(n):
                 above = matrix[row-1][col]
-                
-                if 0 <= col-1 < n:
-                    left_dg = matrix[row-1][col-1]
-                else:
-                    left_dg = math.inf
-                if 0 <= col+1 < n:
-                    right_dg = matrix[row-1][col+1]
-                else:
-                    right_dg = math.inf
-                    
+                left_dg = matrix[row-1][col-1] if 0 <= col-1 < n else math.inf
+                right_dg = matrix[row-1][col+1] if 0 <= col+1 < n else math.inf   
                 matrix[row][col] += min(above,left_dg,right_dg)
                 
         return min(matrix[n-1])
