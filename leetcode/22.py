73b9d17c7ad3759a51c67654e0c6bb8f64470d25
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-06 04:38:32 +0900

    Create 22.py

---
+++ leetcode/22.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.acc = []
+        def helper(mod_str,opening,closing):
+            
+            if opening == closing == n:
+                self.acc.append(mod_str)
+                return
+            
+            if opening < n:
+                helper(mod_str + '(',opening + 1,closing)
+            if closing < opening:
+                helper(mod_str + ')',opening,closing + 1)
+                
+            
+        
+        helper('',0,0)       
+        return self.acc
