3cf57993e9ced90d003b646a09c11f1a636e8bdf
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-05 16:46:01 +0900

    Update 983.py

--- leetcode/983.py
+++ leetcode/983.py
@@ -3,19 +3,18 @@ class Solution:
         DAY_PASS,WEEK_PASS,MONTH_PASS = costs[0],costs[1],costs[2]
         
         @lru_cache(None)
-        def dp(i,hasPass,day):
+        def dp(i,day):
             if i == len(days):
                 return 0
             
-            if not hasPass:
-                #have an option to buy some pass
-                option1 = DAY_PASS + dp(i+1,False,0)
-                option2 = WEEK_PASS + dp(i+1,True,days[i] + 7)
-                option3 = MONTH_PASS + dp(i+1,True,days[i] + 30)
+            if day == -1: # means no pass, so needs a new one
+                option1 = DAY_PASS + dp(i+1,-1)
+                option2 = WEEK_PASS + dp(i+1,days[i] + 7)
+                option3 = MONTH_PASS + dp(i+1,days[i] + 30)
                 return min(option1,option2,option3)
             else:
                 if days[i] >= day:
-                    return dp(i,False,0)
-                return dp(i+1,True,day)
+                    return dp(i,-1)
+                return dp(i+1,day)
             
-        return dp(0,False,0)
+        return dp(0,-1)
