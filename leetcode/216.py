2232aaa0ac9d41742dddeabc1b6e23462d69f491
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-10 11:13:23 +0900

    Update 216.py

--- leetcode/216.py
+++ leetcode/216.py
@@ -1,7 +1,7 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
-        def backtrack(choice_list,k):
+        def helper(choice_list,k):
             if k == 1:
                 return [[nums] for nums in choice_list]
             
@@ -10,7 +10,7 @@ class Solution:
             for i in range(len(choice_list)):
                 the_rest = choice_list[i+1:]
                 
-                for combs in backtrack(the_rest,k-1):
+                for combs in helper(the_rest,k-1):
                     res_lst = [choice_list[i]] + combs
                     if sum(res_lst) > n:
                         continue
@@ -19,7 +19,7 @@ class Solution:
             return res
         
         choice_list = [i for i in range(1,10)]
-        for cand in backtrack(choice_list,k):
+        for cand in helper(choice_list,k):
             if sum(cand) == n:
                 res.append(cand)
         return res
