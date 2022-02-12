08fff2112473c40c1a068d0b5525863dcb8d8fc6
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-12 15:53:31 +0900

    Create 52.py

---
+++ leetcode/52.py
@@ -0,0 +1,26 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col,posDiag,negDiag = set(),set(),set()
+        self.count = 0
+        
+        
+        def backtrack(r):
+            if r == n:
+                self.count += 1
+            
+            for c in range(n):
+                if c in col or r+c in negDiag or r-c in posDiag:
+                    continue
+                
+                col.add(c)
+                posDiag.add(r-c)
+                negDiag.add(r+c)
+                
+                backtrack(r+1)
+                
+                col.remove(c)
+                posDiag.remove(r-c)
+                negDiag.remove(r+c)
+        
+        backtrack(0)
+        return self.count
