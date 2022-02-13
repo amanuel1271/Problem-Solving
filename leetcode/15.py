44fd99088db8e595d084bbcf84c84f7905acc3a8
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-14 03:30:14 +0900

    Update 15.py

--- leetcode/15.py
+++ leetcode/15.py
@@ -1,27 +1,28 @@
-class Solution:
-    def twoSum(self,nums,target):
-        res = []
-        seen = {}
-        for i in range(len(nums)):
-            if target - nums[i] in seen:
-                res.append([nums[i],target - nums[i]])
-            seen[nums[i]] = i
-                
-        return res
-            
-        
+class Solution:   
     def threeSum(self, nums: List[int]) -> List[List[int]]: ## target  is zero
+        def twoSum(nums,target):
+            res = []
+            seen = set()
+            for i in range(len(nums)):
+                if target - nums[i] in seen:
+                    res.append([nums[i],target - nums[i]])
+                seen.add(nums[i])
+
+            return res
+        
         res = []
-        seen = {}
+        visited = set()
+        seen = set()
+        
         for i in range(len(nums)):
             if nums[i] in seen:
                 continue
-
-            for j in self.twoSum(nums[i+1:],-nums[i]):
+            for j in twoSum(nums[i+1:],-nums[i]):
                 x = sorted([nums[i]] + j)
-                if x not in res:
+                if (x[0],x[1],x[2]) not in visited:
                     res.append(x)
-            seen[nums[i]] = i
+                    visited.add((x[0],x[1],x[2]))
+            seen.add(nums[i])
                     
         return res
             
