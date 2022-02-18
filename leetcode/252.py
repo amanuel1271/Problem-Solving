19cffc3bd293bbf7c5feb9468270e753fa5f45bb
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-18 11:27:24 +0900

    Update 252.py

--- leetcode/252.py
+++ leetcode/252.py
@@ -7,7 +7,7 @@ class Solution:
         
         minStart,maxEnd = intervals[0]
         for start,end in intervals[1:]:
-            if start >= minStart and end <= maxEnd or start < minStart and end > maxEnd or minStart <= start < maxEnd and end > maxEnd or minStart < end <= maxEnd and start < minStart:
+            if (start >= minStart and end <= maxEnd) or (start < minStart and end > maxEnd) or (minStart <= start < maxEnd and end > maxEnd) or (minStart < end <= maxEnd and start < minStart):
                 return False
             
             minStart,maxEnd = min(start,minStart),max(end,maxEnd)
