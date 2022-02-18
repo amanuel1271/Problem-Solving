8e4de61dbd1eb2def82940566c1cce2bd72ba927
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-18 11:18:00 +0900

    Create 252.py

---
+++ leetcode/252.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals:
+            return True
+        
+        intervals.sort(key = lambda x : x[0])
+        
+        minStart,maxEnd = intervals[0]
+        for start,end in intervals[1:]:
+            if start >= minStart and end <= maxEnd or start < minStart and end > maxEnd or minStart <= start < maxEnd and end > maxEnd or minStart < end <= maxEnd and start < minStart:
+                return False
+            
+            minStart,maxEnd = min(start,minStart),max(end,maxEnd)
+        
+        return True
