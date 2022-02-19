709727e800eec57c4c54d7ae0dd73deccf535431
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-19 13:35:49 +0900

    Create 259.py

---
+++ leetcode/259.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        
+        def twoSmaller(start,thresh):
+            count,left,right = 0,start,n-1
+            
+            while left < right:
+                if (nums[left] + nums[right] < thresh):
+                    count += right - left
+                    left += 1
+                else:
+                    right -= 1
+            
+            return count
+        
+        nums.sort()
+        count  = 0
+        for i in range(n-2):
+            count += twoSmaller(i+1,target-nums[i])
+        
+        return count
