162d0a7a75913407d9bbb8edc3bed16720728248
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-02-25 16:55:20 +0900

    Create 611.py

---
+++ leetcode/611.py
@@ -0,0 +1,14 @@
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(len(nums)-1,1,-1):
+            left = 0
+            right = i - 1
+            while left < right:
+                if (nums[left] + nums[right]) > nums[i]:
+                    ans += (right - left)
+                    right -= 1
+                else:
+                    left += 1
+        return ans
