8d6714996685b97df62c394e5f7cda58cb8da0b1
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-04 21:16:04 +0900

    Create 1387.py

---
+++ leetcode/1387.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        def power_val(x):
+            count = 0
+            while x != 1:
+                if x % 2 == 0:
+                    x = x // 2
+                else:
+                    x = 1 + 3 * x
+                count += 1
+                
+            return count
+                
+        return sorted([num for num in range(lo,hi+1)],key = lambda x: power_val(x))[k-1]
