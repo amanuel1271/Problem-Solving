3e7f2ad465ee1f82bbd954ef71eb39f8a16469b4
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 05:03:37 +0900

    Time: 430 ms (51.70%), Space: 21.4 MB (26.52%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,30 @@
+
+
+class Solution:
+    def numIslands(self, grid):
+        #  attempt using stack and dfs
+        s = {(i,j) for i,row in enumerate(grid) for j, col in enumerate(row) if col == '1'}
+        num = 0 
+        
+        while s:
+            num += 1
+            x,y = s.pop()
+            stack = deque([(x,y)])
+            
+            while stack:
+                x,y = stack[-1]
+                if (x + 1,y) not in s and (x - 1,y) not in s and (x,y + 1) not in s and (x,y - 1) not in s:
+                    stack.pop()
+                for nei in [(x - 1,y),(x + 1,y), (x, y - 1), (x, y + 1)]:
+                    if nei in s:
+                        stack.append(nei)
+                        s.remove(nei)
+                        break
+                        
+        return num
+                
+                
+                    
+                
+        
+        
