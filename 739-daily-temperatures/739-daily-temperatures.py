a0675e1974035d67881acc49ced514b02100dbb6
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 05:32:02 +0900

    Time: 1622 ms (50.51%), Space: 25.2 MB (46.07%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = deque()
+        
+        for curr_day, curr_temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < curr_temp:
+                prev_day = stack.pop()
+                answer[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
+        
+        return answer
+        
