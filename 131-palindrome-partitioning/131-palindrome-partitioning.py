a30475e6b4360a7f2c8a172cea36d95860fefb13
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 04:57:06 +0900

    Time: 865 ms (53.75%), Space: 30.5 MB (37.78%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,31 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        
+        def isPalindrome(string):
+            size = len(string)
+            l,r = 0,size-1
+            
+            while l < r:
+                if string[l] != string[r]:
+                    return False
+                l,r = l+1,r-1
+            return True
+
+        def dfs(string,acc):
+            size = len(string)
+            if size == 0:
+                if acc != []:
+                    res.append(acc)
+                return
+            
+            for i in range(1,len(string) + 1):
+                if isPalindrome(string[:i]):
+                    dfs(string[i:],acc + [string[:i]])
+        
+        dfs(s,[])
+        return res
+                    
+                
+                
+        
