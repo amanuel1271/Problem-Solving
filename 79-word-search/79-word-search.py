b4b195a738f3338904b2c6acbbbfcb3f35c5bbc9
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 04:40:00 +0900

    Time: 7244 ms (52.84%), Space: 14 MB (42.43%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,27 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m,n = len(board),len(board[0])
+        
+        def dfs(x,y,i,path):
+            if i == len(word)-1:
+                return True
+            for xl,xr in [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]:
+                if 0 <= xl < m and 0 <= xr < n and (xl,xr) not in path:
+                    if board[xl][xr] == word[i+1]:
+                        if dfs(xl,xr,i+1,set.union(path,{(x,y)})):
+                            return True
+            return False
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    if dfs(i,j,0,set()):
+                        return True
+        
+        return False
+                    
+            
+                
+            
+        
