788da35bfb95ad55fe7320cd183a197d3382cc52
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 05:20:09 +0900

    Time: 64 ms (6.32%), Space: 13.8 MB (98.94%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s):
+        stk, ans, n = deque(), "", ""
+        for c in s:
+            if c.isalpha():   
+                ans += c
+            elif c.isdigit(): 
+                n += c
+            elif c == '[': 
+                stk.append((n, ans))
+                n, ans = "", ""
+            else:
+                cnt, prev = stk.pop()
+                ans = prev + ans * int(cnt)
+        return ans
+                    
+                    
+                
