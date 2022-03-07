0d1486b0ee1867dd371c302af7da803b9fded70c
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 04:34:52 +0900

    Time: 207 ms (13.37%), Space: 15.8 MB (28.75%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m,n = len(grid),len(grid[0])
+        dp = [[math.inf] * (n+1) for _ in range(m+1)]
+        
+        dp[1][1] = grid[0][0]
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if (i,j) == (1,1):
+                    continue
+                dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + grid[i-1][j-1]
+        
+        return dp[m][n]
+        
+        
