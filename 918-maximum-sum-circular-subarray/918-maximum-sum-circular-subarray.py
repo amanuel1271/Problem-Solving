a1b727ffe0e8c6f70bcf81f1b87ea4672c3db9be
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 05:39:38 +0900

    Time: 1108 ms (8.00%), Space: 19.1 MB (36.34%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxSubarraySumCircular(self, A):
+        
+        def kadane(gen):
+            # Maximum non-empty subarray sum
+            ans = cur = -math.inf
+            for x in gen:
+                cur = x + max(cur, 0)
+                ans = max(ans, cur)
+            return ans
+        
+        S = sum(A)
+        if len(A) == 1:
+            return S
+        
+        ans1 = kadane(iter(A))
+        ans2 = S + kadane(-A[i] for i in range(1, len(A)))
+        ans3 = S + kadane(-A[i] for i in range(len(A) - 1))
+        return max(ans1, ans2, ans3)
+        
