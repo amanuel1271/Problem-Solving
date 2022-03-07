087f3ac4172b94011232ba8082faf2de067b6857
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 04:44:02 +0900

    Time: 80 ms (52.57%), Space: 14.8 MB (98.11%) - LeetHub

---
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        @lru_cache(None)
+        def generate_trees(start, end):
+            if start > end:
+                return [None]
+            
+            all_trees = []
+            for i in range(start, end + 1):
+
+                left_trees = generate_trees(start, i - 1)
+                
+
+                right_trees = generate_trees(i + 1, end)
+                
+
+                for l in left_trees:
+                    for r in right_trees:
+                        current_tree = TreeNode(i)
+                        current_tree.left = l
+                        current_tree.right = r
+                        all_trees.append(current_tree)
+            
+            return all_trees
+        
+        return generate_trees(1, n) if n else []
+        
