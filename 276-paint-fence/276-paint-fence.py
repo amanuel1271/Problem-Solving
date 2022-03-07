5a80d7de64eabfe35634d6c767c81dbd805e064d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 05:14:45 +0900

    Time: 45 ms (43.34%), Space: 13.8 MB (99.11%) - LeetHub

---
+++ 276-paint-fence/276-paint-fence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        def total_ways(i):
+            if i == 1:
+                return k
+            if i == 2:
+                return k * k
+            
+            # Check if we have already calculated totalWays(i)
+            if i not in memo:
+                memo[i] = (k - 1) * (total_ways(i - 1) + total_ways(i - 2))
+            return memo[i]
+
+        memo = {}
+        return total_ways(n)
+        
