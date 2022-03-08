6d8d00ac26ecf13ecb8e5ad49a6f0153e17de63d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 16:20:33 +0900

    Update 45-jump-game-ii.py

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -5,15 +5,11 @@ class Solution:
         
         dp[-1] = 0 # base case
         
-        for j in range(n-2,-1,-1):
-            if nums[j] == 0:
-                continue
-                
+        for j in range(n-2,-1,-1):  
             min_ = math.inf
             for i in range(1,nums[j] + 1):
                 if j+i < n:
-                    min_ = min(min_,dp[j+i] + 1)
-                    
+                    min_ = min(min_,dp[j+i] + 1)    
             dp[j] = min_
             
             
