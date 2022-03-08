be3aa1fd050db6aa20fa6d5631e24682ec58d287
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-08 15:40:30 +0900

    Update 55-jump-game.py

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,28 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        store_bad_indice = dict()
+        goal = n-1
         
-        def dfs(i):
-            if i == n-1:
-                return True
-            elif i > n-1:
-                return False
-            elif i in store_bad_indice:
-                return False
-            
-            for j in range(1,nums[i] + 1):
-                if i+j in store_bad_indice:
-                    continue
-                if dfs(i+j):
-                    return True
-                else:
-                    store_bad_indice[i+j] = 0
-            
-            store_bad_indice[i] = 0
-            return False
-        
-        return dfs(0)
+        for i in range(n-1,-1,-1):
+            if i + nums[i] >= goal:
+                goal = i
+                
+        return goal == 0
             
             
         
