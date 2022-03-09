b9e4b86db8b2f1b53c04c093257b9887bae32cdd
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-09 20:53:36 +0900

    Time: 64 ms (6.71%), Space: 13.9 MB (85.52%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -1,10 +1,11 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         strRep = list(str(num))
+        size = len(strRep)
         maxx = num
         
-        for i in range(len(strRep)):
-            for j in range(i+1,len(strRep)):
+        for i in range(size):
+            for j in range(i+1,size):
                 strRep[i],strRep[j] = strRep[j],strRep[i]
                 maxx = max(maxx,int(''.join(strRep)))
                 strRep[i],strRep[j] = strRep[j],strRep[i]
