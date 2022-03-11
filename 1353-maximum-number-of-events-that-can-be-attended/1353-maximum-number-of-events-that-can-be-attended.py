b9763d13b3740f9140766c585a830f1af4e42a41
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-11 10:42:57 +0900

    Time: 1438 ms (66.15%), Space: 61.5 MB (63.08%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        events.sort()
+        heap = []
+        n = max(events, key=lambda x: x[1])[1]
+        
+        cnt = 0
+        i = 0
+        for day in range(1, n+1):
+            while i < len(events) and events[i][0] == day:
+                heappush(heap, events[i][1])
+                i += 1
+
+            while heap and heap[0] < day:
+                heappop(heap)
+
+            if heap:
+                curr = heappop(heap)
+                cnt += 1
+
+        return cnt
+    
+            
+            
+                    
+            
+            
