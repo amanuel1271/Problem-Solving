9ac0905bc8fa309745fb0ba62647fe2bece071f5
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-13 03:56:24 +0900

    Time: 53 ms (32.45%), Space: 14.6 MB (6.79%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -1,21 +1,21 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        dp = { (len(s), 0) : True } # key=(i, leftCount) -> isValid
+        #dp = { (len(s), 0) : True } # key=(i, leftCount) -> isValid
+        @lru_cache(None)
         def dfs(i, left):
             if i == len(s) or left < 0:
                 return left == 0
-            if (i, left) in dp:
-                return dp[(i, left)]
+            
             
             if s[i] == "(":
-                dp[(i, left)] = dfs(i + 1, left + 1)
+                res = dfs(i + 1, left + 1)
             elif s[i] == ")":
-                dp[(i, left)] =  dfs(i + 1, left - 1)
+                res =  dfs(i + 1, left - 1)
             else:
-                dp[(i, left)] =  (dfs(i + 1, left + 1) or
+                res =  (dfs(i + 1, left + 1) or
                                   dfs(i + 1, left - 1) or
                                   dfs(i + 1, left))
-            return dp[(i, left)]
+            return res
         
         return dfs(0, 0)
                     
