412af8b0c855610aa4ce8ac7fb8b0131ad04f001
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-19 04:13:22 +0900

    Time: 296 ms (27.52%), Space: 14.5 MB (28.57%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        @lru_cache(None)
+        def dp(i, j):
+            if i == j:
+                return 0
+            
+            min_val = math.inf
+            for k in range(i, j):
+                
+                left_max = max(arr[i:k+1])
+                right_max = max(arr[k+1:j+1])
+
+                min_val = min(min_val,  left_max * right_max  + dp(i, k) + dp(k+1, j))
+
+            return min_val
+        
+        return dp(0, len(arr)-1)
