9a05c4edb58dc5207526957a7f39a79e850129cb
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-18 10:21:31 +0900

    Time: 3143 ms (5.01%), Space: 32.6 MB (56.33%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -3,22 +3,25 @@ class Solution:
         size = len(changed)
         minheap = changed[:]
         heapify(minheap)
+        seen = dict()
         
-        original,seen = [],dict()
+        for num in changed:
+            if num not in seen:
+                seen[num] = 0
+            
+        
+        original = []
         
         while minheap:
             number = heappop(minheap)
-            
-            if number not in seen:
-                seen[number] = 0
+    
   
             if not (number % 2 == 0 and number//2 in seen and seen[number//2] > 0):            
                 original.append(number)
                 seen[number] += 1
             elif number % 2 == 0 and (number//2 in seen) and  seen[number//2] > 0:
                 seen[number//2] -= 1
-
-        print(original)    
+ 
         return original if size % 2 == 0 and len(original) * 2 == size else []
         
             
