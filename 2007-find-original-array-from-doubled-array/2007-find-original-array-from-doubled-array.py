f1caf2f6123a4b309552341ddbf5a3e5ea37adea
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-18 10:34:37 +0900

    Time: 1744 ms (68.28%), Space: 32.6 MB (59.64%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -6,11 +6,12 @@ class Solution:
         
         while minheap:
             number = heappop(minheap)
-            if not (number % 2 == 0 and seen[number//2] > 0):            
+            if number % 2 == 0 and seen[number//2] > 0:
+                seen[number//2] -= 1
+            else:
                 original.append(number)
                 seen[number] += 1
-            else:
-                seen[number//2] -= 1
+                
  
         return original if size % 2 == 0 and len(original) * 2 == size else []
         
