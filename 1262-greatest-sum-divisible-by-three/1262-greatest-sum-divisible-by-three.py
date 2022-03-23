538e9004f6722116afb581b256574bcfaf8a55d5
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-23 17:43:21 +0900

    Time: 398 ms (62.58%), Space: 19.2 MB (88.39%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -4,7 +4,6 @@ class Solution:
         
         for col in range(len(nums)):
             first,second,third = nums[col] + dp[0],nums[col] + dp[1],nums[col] + dp[2]
-            
             dp[first%3] = max(dp[first%3],first)
             dp[second%3] = max(dp[second%3],second)
             dp[third%3] = max(dp[third%3],third)
