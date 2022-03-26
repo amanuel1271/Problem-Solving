f3f529d3d7405f4ae36cfebeb1446c67023de4a8
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-26 23:37:41 +0900

    Time: 462 ms (16.71%), Space: 16.6 MB (79.28%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,8 +1,7 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count,cum_sum = 0,0
-        hashmap = dict()
-        hashmap[0] = 1
+        hashmap = {0:1}
         
         for i in range(len(nums)):
             cum_sum += nums[i]
