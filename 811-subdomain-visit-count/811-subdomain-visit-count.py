ca4ff30eb99c12deb8753437451c07143b899fda
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-03-28 08:57:41 +0900

    Time: 84 ms (40.44%), Space: 13.8 MB (98.92%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.py
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -1,18 +1,15 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
         domain_to_count = {}
+        ans = []
         
-        def parse_inner(string):
+        for string in cpdomains:
             count,domain = string.split()
             subdomains = domain.split('.')
-            
             for i in range(len(subdomains)):
                 dom = '.'.join(subdomains[i:])
                 domain_to_count[dom] = domain_to_count.get(dom,0) + int(count)
-            
-        ans = []
-        for domains in cpdomains:
-            parse_inner(domains) 
+        
         for dom,count in domain_to_count.items():
             ans.append(str(count) + ' ' + dom)
         
