aced01b4dd9f83d8fb98c06f5a916da73c058095
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-02 04:43:10 +0900

    Time: 198 ms (8.88%), Space: 21.9 MB (64.25%) - LeetHub

--- 244-shortest-word-distance-ii/244-shortest-word-distance-ii.py
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.py
@@ -9,10 +9,11 @@ class WordDistance:
 
     def shortest(self, word1: str, word2: str) -> int:
         loc1, loc2 = self.locations[word1], self.locations[word2]
+        size1,size2 = len(loc1),len(loc2)
         l1, l2 = 0, 0
         min_diff = math.inf
 
-        while l1 < len(loc1) and l2 < len(loc2):
+        while l1 < size1 and l2 < size2:
             min_diff = min(min_diff, abs(loc1[l1] - loc2[l2]))
             if loc1[l1] <= loc2[l2]:
                 l1 += 1
