d99678c04cad511153107b457806e2d892449c4e
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-07 20:12:02 +0900

    Update 1046-last-stone-weight.py

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -3,15 +3,15 @@ class Solution:
         stones_size = len(stones)
         for i in range(stones_size):
             stones[i] *= -1
-        heapify(stones)
+        heapq.heapify(stones)
         
         while stones_size > 1:
-            y = -1 * heappop(stones)
-            x = -1 * heappop(stones)
+            y = -1 * heapq.heappop(stones)
+            x = -1 * heapq.heappop(stones)
             stones_size -= 2
             
             if x != y:
-                heappush(stones,x-y)
+                heapq.heappush(stones,x-y)
                 stones_size += 1
                 
         return 0 if stones_size == 0 else -1 * stones[0]
