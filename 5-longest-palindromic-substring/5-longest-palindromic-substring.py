e58c4adc37cca298003b63d3c794a44184be9834
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-12 08:32:40 +0900

    Time: 1295 ms (56.26%), Space: 13.9 MB (93.10%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -3,14 +3,10 @@ class Solution:
         max_len,max_str = 0,''
         s_size = len(s)
         
-        #aba abba
-        
         for mid in range(s_size):
             l,r = mid-1,mid+1
-            
             if max_len == 0:
-                max_len = 1
-                max_str = s[mid]
+                max_len,max_str = 1,s[mid]
             
             while l >= 0 and r < s_size:
                 if s[l] != s[r]:
@@ -27,6 +23,4 @@ class Solution:
                     max_len,max_str = r-l+1,s[l:r+1]
                 l,r = l-1,r+1
             
-            
-        
         return max_str 
