8f435225fc1bc442c38a63361039de7b11b94a65
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-12 06:47:37 +0900

    Time: 120 ms (73.45%), Space: 15.9 MB (21.92%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -3,20 +3,16 @@ class Solution:
 
         size = len(height)
         left_end,right_end = [0 for _ in range(size)],[0 for _ in range(size)]
-
-        
         cur_max_left,cur_max_right = 0,0
         
         for i in range(size):
             cur_max_left = max(cur_max_left,height[i])
             left_end[i] = cur_max_left
-            
         for j in range(size-1,-1,-1):
             cur_max_right = max(cur_max_right,height[j])
             right_end[j] = cur_max_right
             
         count = 0
-        
         for i in range(size):
             minn = min(left_end[i],right_end[i])
             count += minn - height[i]
