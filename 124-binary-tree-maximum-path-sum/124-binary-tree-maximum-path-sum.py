902d4ed7f9c444705eaca9d4ac8cea536a36b1da
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-14 13:33:40 +0900

    Time: 94 ms (82.63%), Space: 21.4 MB (64.91%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -10,21 +10,15 @@ class Solution:
             if not node:
                 return 0
 
-
             left_gain = max(max_gain(node.left), 0)
             right_gain = max(max_gain(node.right), 0)
-            
-            # the price to start a new path where `node` is a highest node
             price_newpath = node.val + left_gain + right_gain
-            
-            # update max_sum if it's better to start a new path
             self.max_sum = max(self.max_sum, price_newpath)
         
-            # for recursion :
-            # return the max gain if continue the same path
+
             return node.val + max(left_gain, right_gain)
    
-        self.max_sum = float('-inf')
+        self.max_sum = -math.inf
         max_gain(root)
         return self.max_sum
         
