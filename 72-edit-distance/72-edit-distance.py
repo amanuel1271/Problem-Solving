19da573d65758fc3078bd71a879fd1ea77a2c364
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-16 08:50:45 +0900

    Time: 182 ms (73.20%), Space: 17.6 MB (66.94%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -17,25 +17,5 @@ class Solution:
         
         return dp[0][0]
         
-#         @lru_cache(None)
-#         def helper(i,j):
-#             if i == len(word1) and j == len(word2):
-#                 return 0
-#             if i == len(word1):
-#                 return len(word2) - j
-#             if j == len(word2):
-#                 return len(word1) - i
 
-
-#             if word1[i] == word2[j]:
-#                 ans = helper(i + 1, j + 1)
-#             else: 
-#                 insert = 1 + helper(i, j + 1)
-#                 delete = 1 + helper( i + 1, j)
-#                 replace = 1 + helper(i + 1, j + 1)
-#                 ans = min(insert, delete, replace)
-                
-#             return ans
-        
-#         return helper(0,0)
         
