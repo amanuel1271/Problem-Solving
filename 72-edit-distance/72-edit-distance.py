4ffd3f59eec46580efe9ca3d42a52e8295c8444a
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-16 08:36:48 +0900

    Time: 95 ms (98.04%), Space: 17.2 MB (76.16%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,8 +1,8 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
-        
-        def helper(i,j,memo):
+        @lru_cache(None)
+        def helper(i,j):
             if i == len(word1) and j == len(word2):
                 return 0
             if i == len(word1):
@@ -11,17 +11,15 @@ class Solution:
                 return len(word1) - i
 
 
-            if (i, j) not in memo:
-                if word1[i] == word2[j]:
-                    ans = helper(i + 1, j + 1, memo)
-                else: 
-                    insert = 1 + helper(i, j + 1, memo)
-                    delete = 1 + helper( i + 1, j, memo)
-                    replace = 1 + helper(i + 1, j + 1, memo)
-                    ans = min(insert, delete, replace)
-                memo[(i, j)] = ans
+            if word1[i] == word2[j]:
+                ans = helper(i + 1, j + 1)
+            else: 
+                insert = 1 + helper(i, j + 1)
+                delete = 1 + helper( i + 1, j)
+                replace = 1 + helper(i + 1, j + 1)
+                ans = min(insert, delete, replace)
                 
-            return memo[(i, j)]
+            return ans
         
-        return helper(0,0,{})
+        return helper(0,0)
         
