b877ba1ef15a23ed87c7243cda27891dd905711d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-18 14:04:36 +0900

    Time: 146 ms (91.27%), Space: 22.2 MB (35.69%) - LeetHub

--- 403-frog-jump/403-frog-jump.py
+++ 403-frog-jump/403-frog-jump.py
@@ -4,20 +4,9 @@ class Solution:
         if stones[1] > 1:
             return False
         
-        def binary_search(arr,target):
-            l,size = 0,len(arr)
-            r = size - 1
-            
-            while l <= r:
-                mid = (l+r)//2
-                if arr[mid] == target:
-                    return mid
-                elif target > arr[mid]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-                    
-            return -1
+        store_ind = {}
+        for i in range(len(stones)):
+            store_ind[stones[i]] = i
         
                 
             
@@ -30,11 +19,12 @@ class Solution:
             for option in [last_jump-1,last_jump,last_jump+1]:
                 if option >= 1:
                     target = option + stones[i]
-                    search_i = binary_search(stones[i+1:],target)
-                    if search_i == -1:
+                    indice = -1
+                    if target in store_ind:
+                        indice = store_ind[target]
+                    else:
                         continue
-
-                    if cross(i + search_i + 1,option):
+                    if cross(indice,option):
                         return True
                 
             return False
