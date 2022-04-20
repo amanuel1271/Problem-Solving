5d4d89477dbfe454742e75edd6dfcae0ea8e6184
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-20 17:42:30 +0900

    Time: 60 ms (60.62%), Space: 14.4 MB (87.29%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,20 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alpha_num = ''
-        for ch in s:
-            if ch.isalpha():
-                alpha_num += ch.lower()
-            elif ch.isdigit():
-                alpha_num += ch
-                
-        
-        l,r = 0,len(alpha_num)-1
+        l,r = 0,len(s)-1
         
         while l <= r:
-            if alpha_num[l] != alpha_num[r]:
-                return False
-            r -= 1
-            l += 1
+            if not (s[l].isalpha() or s[l].isdigit()) and (s[r].isalpha() or s[r].isdigit()):
+                l += 1
+            elif not (s[r].isalpha() or s[r].isdigit()) and (s[l].isalpha() or s[l].isdigit()):
+                r -= 1
+            elif not (s[r].isalpha() or s[r].isdigit()) and not (s[l].isalpha() or s[l].isdigit()):
+                l += 1
+                r -= 1
+                
+            elif (s[r].isalpha() or s[r].isdigit()) and (s[l].isalpha() or s[l].isdigit()):
+                if s[l].lower() != s[r].lower():
+                    return False
+                r -= 1
+                l += 1
+                
             
         return True
                 
