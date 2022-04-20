e0f7315bb49fe45ec5fa1775da67a28f7a181a12
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-20 17:44:02 +0900

    Time: 103 ms (12.09%), Space: 14.4 MB (87.29%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -3,15 +3,15 @@ class Solution:
         l,r = 0,len(s)-1
         
         while l <= r:
-            if not (s[l].isalpha() or s[l].isdigit()) and (s[r].isalpha() or s[r].isdigit()):
+            if not (s[l].isalnum()) and (s[r].isalnum()):
                 l += 1
-            elif not (s[r].isalpha() or s[r].isdigit()) and (s[l].isalpha() or s[l].isdigit()):
+            elif not (s[r].isalnum()) and (s[l].isalnum()):
                 r -= 1
-            elif not (s[r].isalpha() or s[r].isdigit()) and not (s[l].isalpha() or s[l].isdigit()):
+            elif not (s[r].isalnum()) and not (s[l].isalnum()):
                 l += 1
                 r -= 1
                 
-            elif (s[r].isalpha() or s[r].isdigit()) and (s[l].isalpha() or s[l].isdigit()):
+            else:
                 if s[l].lower() != s[r].lower():
                     return False
                 r -= 1
