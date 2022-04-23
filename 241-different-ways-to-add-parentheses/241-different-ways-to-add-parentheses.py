892a7997c7a84d81a42c8282e11a3b5e75dfe14c
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-23 12:16:40 +0900

    Time: 39 ms (79.67%), Space: 14.1 MB (57.15%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
@@ -1,32 +1,29 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        self.memo = defaultdict(list)
-        return self.ComputeUtil(expression)
         
+        @lru_cache(None)
+        def helper(expression):
+
+            if ('+' not in expression) and ('-' not in expression) and ('*' not in expression):
+                return [int(expression)]
+
+            res = []
+
+            for i, v in enumerate(expression):
+                if v == '+' or v == '-' or v == '*':
+                    left_res = helper(expression[:i])
+                    right_res = helper(expression[i + 1:])
+                    for left_i, left_v in enumerate(left_res):
+                        for right_i, right_v in enumerate(right_res):
+                            if v == '+':
+                                res.append(left_v + right_v)
+                            elif v == '-':
+                                res.append(left_v - right_v)
+                            else:
+                                res.append(left_v * right_v)
+            return res
         
-    def ComputeUtil(self, expression: str) -> List[int]:
-        if expression in self.memo:
-            return self.memo[expression]
-        
-        if ('+' not in expression) and ('-' not in expression) and ('*' not in expression):
-            return [int(expression)]
-        
-        res = []
-        
-        for i, v in enumerate(expression):
-            if v == '+' or v == '-' or v == '*':
-                left_res = self.ComputeUtil(expression[:i])
-                right_res = self.ComputeUtil(expression[i + 1:])
-                for left_i, left_v in enumerate(left_res):
-                    for right_i, right_v in enumerate(right_res):
-                        if v == '+':
-                            res.append(left_v + right_v)
-                        elif v == '-':
-                            res.append(left_v - right_v)
-                        else:
-                            res.append(left_v * right_v)
-        self.memo[expression] = res
-        return res
+        return helper(expression)
         
         
         
