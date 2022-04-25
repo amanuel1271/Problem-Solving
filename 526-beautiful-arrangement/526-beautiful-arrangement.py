c10948b3422ec05bdcfff195400c340737c615c7
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-25 20:24:05 +0900

    Time: 112 ms (92.44%), Space: 13.9 MB (37.09%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        S = {i for i in range(1,n+1)}
+        self.ans = 0
+        
+        def backtrack(N, S):
+            if N == 1:
+                self.ans += 1
+                return
+            
+            for i in S:
+                if N%i == 0 or i%N == 0:
+                    backtrack(N-1, S-{i})
+                    
+        backtrack(n, S)
+        return self.ans
+            
+        
