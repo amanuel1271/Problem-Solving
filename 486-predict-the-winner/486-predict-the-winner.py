c266d9e068f7934a8e0e4dd9d052af3c1256eb74
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-26 22:10:31 +0900

    Time: 48 ms (60.98%), Space: 14.3 MB (17.59%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -4,18 +4,18 @@ class Solution:
 
         
         @lru_cache(None)
-        def dfs(i,j,turn):
+        def player_1_profit(i,j,turn):
             if j == i:
                 return turn * nums[j]
             
-            move_1 = turn * nums[i] + dfs(i+1,j,-1 * turn)
-            move_2 = turn * nums[j] + dfs(i,j-1,-1 * turn)
+            move_1 = turn * nums[i] + player_1_profit(i+1,j,-1 * turn)
+            move_2 = turn * nums[j] + player_1_profit(i,j-1,-1 * turn)
             
             return max(move_1,move_2) if turn == 1 else min(move_1,move_2)
                 
         
 
-        return dfs(0,size-1,1) >= 0
+        return player_1_profit(0,size-1,1) >= 0
                 
                 
             
