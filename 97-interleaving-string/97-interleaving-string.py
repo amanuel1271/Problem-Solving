8d206eeccc2d7f2bec521cc7c5c4fce5bc4e3ccc
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-27 03:13:05 +0900

    Time: 69 ms (37.14%), Space: 14.9 MB (38.18%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -6,15 +6,16 @@ class Solution:
         def dfs(i,j,k):
             if i == size_1 and j == size_2 and k == size_3:
                 return True
-
-            option_1,option_2 = False,False
             
             if i < size_1 and k < size_3 and s3[k] == s1[i]:
-                option_1 = dfs(i+1,j,k+1)
+                if dfs(i+1,j,k+1):
+                    return True
             if j < size_2 and k < size_3 and s3[k] == s2[j]:
-                option_2 = dfs(i,j+1,k+1)
+                if dfs(i,j+1,k+1):
+                    return True
                 
-            return option_1 or option_2
+                
+            return False
         
         return dfs(0,0,0)
             
