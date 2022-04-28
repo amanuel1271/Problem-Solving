06a8fc61fa94969b644d15dffce51e367a4bd07d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-29 01:19:48 +0900

    Time: 666 ms (34.07%), Space: 126.5 MB (6.49%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,15 +1,18 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
+        size = len(piles)
         
         @lru_cache(None)
-        def play(i,j,turn):
+        def play(i,j):
             if i > j:
                 return 0
             
-            option_1 = turn * piles[i] + play(i+1,j,-turn)
-            option_2 = turn * piles[j] + play(i,j-1,-turn)
+            turn = size - (j-i+1)
             
-            return max(option_1,option_2) if turn == 1 else min(option_1,option_2)
+            option_1 = turn * piles[i] + play(i+1,j)
+            option_2 = turn * piles[j] + play(i,j-1)
+            
+            return max(option_1,option_2) if turn % 2 == 0 else min(option_1,option_2)
         
-        return play(0,len(piles)-1,1)
+        return play(0,len(piles)-1)
         
