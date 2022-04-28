0663befc4e60f421653e55fda43bf520f6e8597a
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-29 01:22:50 +0900

    Time: 648 ms (35.07%), Space: 124.7 MB (14.71%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -8,9 +8,10 @@ class Solution:
                 return 0
             
             turn = size - (j-i+1)
+            c = 1 if turn % 2 == 0 else -1
             
-            option_1 = turn * piles[i] + play(i+1,j)
-            option_2 = turn * piles[j] + play(i,j-1)
+            option_1 = c * piles[i] + play(i+1,j)
+            option_2 = c * piles[j] + play(i,j-1)
             
             return max(option_1,option_2) if turn % 2 == 0 else min(option_1,option_2)
         
