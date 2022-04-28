c21f3043d58084f0b56799f43c1fd9d402f07dad
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-28 13:50:16 +0900

    Time: 1053 ms (85.84%), Space: 27.5 MB (68.84%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -4,15 +4,20 @@ class Solution:
         score = [0]*n
         score[0] = nums[0]
         dq = deque()
+        
         dq.append(0)
+        
         for i in range(1, n):
-            # pop the old index
+            
             if dq and dq[0] < i-k:
                 dq.popleft()
+                
             score[i] = score[dq[0]] + nums[i]
-            # pop the smaller value
+
             while dq and score[i] >= score[dq[-1]]:
                 dq.pop()
             dq.append(i)
+            
+            
         return score[-1]
         
