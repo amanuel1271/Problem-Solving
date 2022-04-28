213ab78438b7dfcea95e5b3ec3e7979f6ed9e2dc
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-29 01:50:44 +0900

    Time: 43 ms (80.96%), Space: 13.9 MB (49.93%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,21 +1,32 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
-        @lru_cache(None)
-        def add_up(trg):
-            if trg == 0:
-                return 1
-            
-            
-            count = 0
-            
+        count = [0]*(target + 1)
+        count[0] = 1
+        
+        for trg in range(1,target+1):
             for num in nums:
                 if trg-num >= 0:
-                    count += add_up(trg-num)
+                    count[trg] += count[trg-num]
                     
-            return count
+        return count[-1]
+            
         
-        return add_up(target)
+#         @lru_cache(None)
+#         def add_up(trg):
+#             if trg == 0:
+#                 return 1
+            
+            
+#             count = 0
+            
+#             for num in nums:
+#                 if trg-num >= 0:
+#                     count += add_up(trg-num)
+                    
+#             return count
+        
+#         return add_up(target)
                     
         
             
