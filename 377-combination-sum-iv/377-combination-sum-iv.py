d664dce53468d6462078e0f3c5beefb03687f8ad
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-29 01:50:53 +0900

    Time: 42 ms (82.42%), Space: 13.8 MB (99.09%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -11,22 +11,7 @@ class Solution:
                     
         return count[-1]
             
-        
-#         @lru_cache(None)
-#         def add_up(trg):
-#             if trg == 0:
-#                 return 1
-            
-            
-#             count = 0
-            
-#             for num in nums:
-#                 if trg-num >= 0:
-#                     count += add_up(trg-num)
-                    
-#             return count
-        
-#         return add_up(target)
+    
                     
         
             
