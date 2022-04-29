41d1b81335c2878dea7c3ad22736a325bf0ab5ff
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-29 13:57:42 +0900

    Time: 27 ms (96.00%), Space: 14.1 MB (14.30%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,24 +1,14 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        adj_list = {
-            'a':['a','e','i','o','u'],
-            'e': ['e','i','o','u'],
-            'i': ['i','o','u'],
-            'o': ['o','u'],
-            'u': ['u']   
-        }
         
-        def generate(k):
+        @lru_cache(None)
+        def helper(k,vowels):
             if k == 1:
-                return ["a","e","i","o","u"]
+                return vowels
+            if vowels == 1:
+                return 1
             
-            res = []
-            for ch in generate(k-1):
-                for nei in adj_list[ch[-1]]:
-                    res.append(ch + nei)
-            
-            return res
+            return helper(k-1,vowels) + helper(k,vowels-1)
         
-        return len(generate(n))
-                    
+        return helper(n,5)
         
