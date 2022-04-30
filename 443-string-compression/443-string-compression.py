507cda7619663afe90b7ea896466c524f56394ae
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-04-30 09:25:30 +0900

    Time: 80 ms (50.32%), Space: 13.9 MB (97.98%) - LeetHub

---
+++ 443-string-compression/443-string-compression.py
@@ -0,0 +1,46 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        def write(count,cur_index,i):
+            if count == 1:
+                chars[cur_index] = chars[i-1]
+                cur_index += 1
+                return (False,cur_index)
+            else:
+                chars[cur_index] = chars[i-1]
+                cur_index += 1
+                for ch in str(count):
+                    chars[cur_index] = ch
+                    cur_index += 1
+            
+            return (True,cur_index)
+            
+            
+        if len(chars) == 1:
+            return 1
+        
+        cur_index = 0
+        count = 1
+        i,size = 1,len(chars)
+        
+        while i < size:
+            if chars[i] == chars[i-1]:
+                count += 1
+            else:
+                res,cur_index = write(count,cur_index,i)
+                if res:
+                    count = 1
+            
+            i += 1
+        
+        #print(chars,cur_index)
+        res,cur_index = write(count,cur_index,i)
+        for _ in range(size - cur_index):
+            chars.pop()
+            
+        return cur_index
+            
+                    
+        
+                    
+        
+        
