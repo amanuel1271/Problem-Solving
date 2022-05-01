495957e31106571195833a30d676c3f99851634a
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-02 02:18:55 +0900

    Time: 72 ms (25.08%), Space: 13.8 MB (98.41%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -5,8 +5,8 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy_node = ListNode(0,head)
-        fpointer,spointer = dummy_node,dummy_node.next
+        dummy = ListNode(0,head)
+        fpointer,spointer = dummy,dummy.next
         
         while fpointer:
             if spointer and spointer.next and spointer.next.val == spointer.val:
@@ -23,5 +23,5 @@ class Solution:
             
         
     
-        return dummy_node.next
+        return dummy.next
         
