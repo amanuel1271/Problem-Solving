95fffbd3ce41dffd87164e1d4bf1a66b07cff3a8
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-02 02:18:28 +0900

    Time: 75 ms (20.59%), Space: 13.8 MB (98.41%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -6,20 +6,20 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy_node = ListNode(0,head)
-        f_pointer,s_pointer = dummy_node,dummy_node.next
+        fpointer,spointer = dummy_node,dummy_node.next
         
-        while f_pointer:
-            if s_pointer and s_pointer.next and s_pointer.next.val == s_pointer.val:
-                value = s_pointer.val
-                while s_pointer and s_pointer.val == value:
-                    s_pointer = s_pointer.next
+        while fpointer:
+            if spointer and spointer.next and spointer.next.val == spointer.val:
+                value = spointer.val
+                while spointer and spointer.val == value:
+                    spointer = spointer.next
 
-                f_pointer.next = s_pointer
+                fpointer.next = spointer
                 
             else:
-                if s_pointer:
-                    s_pointer = s_pointer.next
-                f_pointer = f_pointer.next
+                if spointer:
+                    spointer = spointer.next
+                fpointer = fpointer.next
             
         
     
