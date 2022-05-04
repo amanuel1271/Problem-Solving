3076554d498948ed5ec27e28e7d431b3fda74553
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-04 20:11:51 +0900

    Time: 52 ms (23.61%), Space: 13.9 MB (34.38%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -0,0 +1,43 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m,n = len(matrix),len(matrix[0])
+        visited = set()
+        next_orientation = {
+            'E' : 'S',
+            'S' : 'W',
+            'W' : 'N',
+            'N' : 'E'
+        }
+        move = {
+            'E' : (0,1),
+            'S' : (1,0),
+            'W' : (0,-1),
+            'N' : (-1,0)
+            
+        }
+        pos = (0,0)
+        orientation = 'E'
+        res = []
+        
+        while True:
+            visited.add(pos)
+            res.append(matrix[pos[0]][pos[1]])
+            
+            nextx,nexty = pos[0] + move[orientation][0],pos[1] + move[orientation][1]
+            if (0 <= nextx < m and 0 <= nexty < n) and (nextx,nexty) not in visited:
+                pos = (nextx,nexty)
+            else:
+                orientation = next_orientation[orientation]
+                nextx,nexty = pos[0] + move[orientation][0],pos[1] + move[orientation][1]
+                if (0 < nextx < m and 0 <= nexty < n) and (nextx,nexty) not in visited:
+                    pos = (nextx,nexty)
+                else:
+                    break
+                    
+        return res
+                
+                
+            
+            
+        
+        
