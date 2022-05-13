7ff2b6ee5e0d9613a17e89de16e53d942d42a81d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-13 12:32:18 +0900

    Time: 32 ms (97.51%), Space: 15.3 MB (48.96%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -8,27 +8,21 @@ class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         def helper(root):
             if not root:
-                return root
-            elif not root.left and not root.right:
+                return None
+            
+            if not root.left and not root.right:
                 return root
             
-
             left,right = helper(root.left),helper(root.right)
-            if not left:
-                root.right = right
-                return root
             
-            save_left = left
-            while save_left.right:
-                save_left = save_left.right
+            if left:
+                left.right = root.right
+                root.right = root.left
+                root.left = None
                 
-            save_left.right = right
-            root.right = left
-            root.left = None
-            return root
+            return right if right != None else left
             
-        
-
+            
         helper(root)
 
                 
