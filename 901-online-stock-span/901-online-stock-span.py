742bf5e3a77d6534619aebc67a2c33e2431da82c
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-14 03:11:43 +0900

    Time: 451 ms (76.45%), Space: 19.5 MB (76.21%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.py
+++ 901-online-stock-span/901-online-stock-span.py
@@ -1,16 +1,17 @@
 class StockSpanner:
 
     def __init__(self):
-        self.stack = [[-math.inf,-1]]
-        self.index = 0
+        self.stack = []
+        self.index = 1
         
 
     def next(self, price: int) -> int:
         ans = [price,self.index]
-        while self.stack and self.stack[-1][0] != -math.inf and self.stack[-1][0] <= price:
+        while self.stack and self.stack[-1][0] <= price:
             self.stack.pop()
             
-        res,self.index = ans[1] - self.stack[-1][1],self.index + 1
+        res = ans[1] - self.stack[-1][1] if self.stack else ans[1]
+        self.index += 1
         self.stack.append(ans) 
         return res
 
