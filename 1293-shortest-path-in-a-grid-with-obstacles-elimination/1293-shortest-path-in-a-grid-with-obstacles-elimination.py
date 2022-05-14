d3edcd58d3a3fcc772a5eddfaef360f88d4d17d6
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-15 07:44:42 +0900

    Time: 708 ms (40.77%), Space: 22.4 MB (14.87%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,35 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        rows, cols = len(grid), len(grid[0])
+        target = (rows - 1, cols - 1)
+
+        
+        state = (0, 0, k)
+        queue = deque([(0, state)])
+        seen = set([state])
+
+        while queue:
+            steps, (row, col, k) = queue.popleft()
+
+            if (row, col) == target:
+                return steps
+
+            for new_row, new_col in [(row, col + 1), (row + 1, col), (row, col - 1), (row - 1, col)]:
+
+                if (0 <= new_row < rows) and (0 <= new_col < cols):
+                    new_eliminations = k - grid[new_row][new_col]
+                    new_state = (new_row, new_col, new_eliminations)
+
+                    if new_eliminations >= 0 and new_state not in seen:
+                        seen.add(new_state)
+                        queue.append((steps + 1, new_state))
+
+
+        return -1
+        
+                
+            
+            
+            
+            
+        
