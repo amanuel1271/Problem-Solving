4d33736baf035a91128a03972750e7e41f31bc50
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-22 00:19:41 +0900

    Time: 53 ms (9.77%), Space: 14 MB (26.99%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         
         res = [[math.inf for _ in range(n)] for _ in range(n)]
-        
         ori = 'E'
         next_ori = {'E':'S','S':'W','W':'N','N':'E'}
         move = {'E':(0,1),'S':(1,0),'W':(0,-1),'N':(-1,0)}
@@ -10,7 +9,7 @@ class Solution:
         
         res[0][0],count = 1,2
         
-        while True:
+        for i in range(2,n*n+1):
             dx,dy = move[ori]
             row_pos,col_pos = row_pos + dx ,col_pos  + dy
             if not (0 <= row_pos < n and 0 <= col_pos < n and res[row_pos][col_pos] == math.inf):
@@ -18,14 +17,11 @@ class Solution:
                 ori = next_ori[ori]
                 dx,dy = move[ori]
                 row_pos,col_pos = row_pos + dx ,col_pos  + dy
-                if not (0 <= row_pos < n and 0 <= col_pos < n and res[row_pos][col_pos] == math.inf):
-                    break
+
             
-                res[row_pos][col_pos] = count
-                count += 1
-            else:
-                res[row_pos][col_pos] = count
-                count += 1
+  
+            res[row_pos][col_pos] = count
+            count += 1
                 
         return res
                 
