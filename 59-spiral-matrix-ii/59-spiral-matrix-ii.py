83c5566c604038fc1ba6ce307af9e5beac71df4d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-21 23:56:24 +0900

    Time: 58 ms (7.08%), Space: 13.9 MB (73.30%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        
+        res = [[math.inf for _ in range(n)] for _ in range(n)]
+        
+        ori = 'E'
+        next_ori = {'E':'S','S':'W','W':'N','N':'E'}
+        move = {'E':(0,1),'S':(1,0),'W':(0,-1),'N':(-1,0)}
+        row_pos,col_pos = 0,0
+        
+        res[0][0],count = 1,2
+        
+        while True:
+            dx,dy = move[ori]
+            row_pos,col_pos = row_pos + dx ,col_pos  + dy
+            if not (0 <= row_pos < n and 0 <= col_pos < n and res[row_pos][col_pos] == math.inf):
+                row_pos,col_pos = row_pos - dx,col_pos  - dy
+                ori = next_ori[ori]
+                dx,dy = move[ori]
+                row_pos,col_pos = row_pos + dx ,col_pos  + dy
+
+                if not (0 <= row_pos < n and 0 <= col_pos < n and res[row_pos][col_pos] == math.inf):
+                    break
+                    
+                res[row_pos][col_pos] = count
+                count += 1
+                
+            else:
+                res[row_pos][col_pos] = count
+                count += 1
+                
+        return res
+                
+                        
+        
+        
+        
