1b3eef68e08449cc67179af4954a3c680047f0c4
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-21 21:47:01 +0900

    Time: 278 ms (16.09%), Space: 14 MB (29.62%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        def mul(digit,string):
+            ans,tens,num = 0,0,ord(digit) - ord('0')
+
+            for ch in string[::-1]:
+                ans += ((ord(ch) - ord('0')) * num) * (10**tens)
+                tens += 1
+                
+            return ans
+        
+        tens = 0
+        ans = 0
+        
+        for ch in num2[::-1]:
+            ans += (mul(ch,num1)) * (10 ** tens)
+            tens += 1
+            
+        return str(ans)
+            
+                
+                
+                
+            
+            
+        
