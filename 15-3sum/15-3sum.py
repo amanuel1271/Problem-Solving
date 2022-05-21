7854cd924983426c8f4bb662625b456033a5f128
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-21 13:05:31 +0900

    Time: 782 ms (84.08%), Space: 17 MB (95.31%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -2,33 +2,27 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]: ## target  is zero
         
         res = []
-        size = len(nums)
         nums.sort()
         
-        for i in range(size-2):
+        for i in range(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-                
-            j = i + 1
-            k = len(nums)-1
-            target = -nums[i]
-            
-            if target < 2*nums[j]:
-                break
-            while j < k:
-                if target == nums[j] + nums[k]:
-                    res.append([nums[i],nums[j],nums[k]])
-                    k -= 1
-                    while j < k-1 and nums[j + 1] == nums[j]:
-                        j += 1
-                    j += 1
-                elif target > nums[j] + nums[k]:
-                    while j < k-1 and nums[j + 1] == nums[j]:
-                        j += 1
-                    j += 1
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1 
+                elif s > 0:
+                    r -= 1
                 else:
-                    k -= 1
-                    
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                        
+                    l += 1
+                    r -= 1
         return res
             
             
