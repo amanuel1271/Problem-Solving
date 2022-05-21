a9153f5bef17bf6f5a608c0f1815c42cb351875f
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-21 22:59:50 +0900

    Time: 97 ms (62.21%), Space: 17.4 MB (55.00%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -14,8 +14,10 @@ class Solution:
                     r = mid - 1    
             return ans
         
+        
         def not_intersect(range1,range2):
             return (range1[0] > range2[1] or range2[0] > range1[1])
+        
         
         greatest_index = binary_search()
         if greatest_index  == -1:
