f4de7ee45f3729903cc7218aaeab789478914b74
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-21 09:44:42 +0900

    Time: 238 ms (45.69%), Space: 20.4 MB (84.73%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,29 +1,16 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         r,c = len(matrix),len(matrix[0])
+        x,y = r-1,0
         
         
-        def vanilla_bs(search_row):
-            left,right = 0,c-1
-            while left <= right:
-                mid = (left+right)//2
-                if matrix[search_row][mid] == target:
-                    return True
-                elif matrix[search_row][mid] > target:
-                    right = mid-1
-                else:
-                    left = mid+1   
-            return False
-        
-        
-        for i in range(r):
-            if matrix[i][0] == target:
+        while x >= 0 and y <= c-1:
+            if matrix[x][y] == target:
                 return True
-            elif matrix[i][0] > target:
-                return False
-            
-            if vanilla_bs(i):
-                return True
-            
+            elif matrix[x][y] > target:
+                x -= 1
+            else:
+                y += 1
+                
         return False
         
