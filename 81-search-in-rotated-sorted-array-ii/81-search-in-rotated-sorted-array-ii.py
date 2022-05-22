0b1af40808c902de472c95b0bdb353ec1193f326
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-22 15:09:35 +0900

    Time: 60 ms (78.24%), Space: 14.5 MB (21.46%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -11,12 +11,12 @@ class Solution:
                 l += 1
                 
             if nums[l] <= nums[mid]: #increasing
-                if nums[l] <= target < nums[mid]:
+                if nums[l] <= target <= nums[mid]:
                     r = mid-1
                 else:
                     l = mid + 1   
             else:
-                if nums[mid] < target <= nums[r]:
+                if nums[mid] <= target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid-1   
