9cef9c585c9bd051f581311cdb6682a78a6bea75
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-22 14:34:19 +0900

    Time: 66 ms (31.17%), Space: 14.4 MB (18.74%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -6,7 +6,6 @@ class Solution:
             mid = (l+r)//2
             if target == nums[mid]:
                 return mid
-                
             elif nums[l] <= nums[mid]: #increasing
                 if nums[l] <= target <= nums[mid]:
                     r = mid-1
@@ -16,7 +15,6 @@ class Solution:
                 if nums[mid] <= target <= nums[r]:
                     l = mid + 1
                 else:
-                    r = mid-1
-                    
+                    r = mid-1   
         return -1
         
