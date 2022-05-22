f3ca8055f8cb6d2eeca6218129ad27c6acd64277
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-22 15:06:24 +0900

    Time: 88 ms (30.64%), Space: 14.5 MB (58.34%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -3,7 +3,7 @@ class Solution:
 
         l, r = 0, len(nums)-1
         while l <= r:
-            mid = l + (r-l)//2
+            mid = (l+r)//2
             if nums[mid] == target:
                 return True
             while l < mid and nums[l] == nums[mid]: # tricky part
