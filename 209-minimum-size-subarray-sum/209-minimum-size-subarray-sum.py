93dea1955e832aa4fc39ee35a4d97c7ecd5b8fc3
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-24 08:11:00 +0900

    Time: 99 ms (54.03%), Space: 16.7 MB (50.75%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         l,r,window_sum = 0,0,nums[0]
+        if nums[-1] >= target:
+            return 1
         
         
         min_len = math.inf
@@ -13,9 +15,7 @@ class Solution:
                 
             window_sum -= nums[l]
             l += 1
-            
-        if window_sum >= target:
-            min_len = min(min_len,r-l+1)  
+             
         return min_len if min_len != math.inf else 0
             
         
