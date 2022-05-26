b67b63088c0fe48b6ed62be3cedcddd6729261c2
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-27 05:22:00 +0900

    Time: 115 ms (8.56%), Space: 44.3 MB (6.42%) - LeetHub

--- 343-integer-break/343-integer-break.js
+++ 343-integer-break/343-integer-break.js
@@ -5,7 +5,6 @@
 var integerBreak = function(n) {
 
     const ans = {};
-    
     const fetchCached = n => {
         if (!(n in ans)){
             ans[n] = helper(n);
@@ -13,24 +12,18 @@ var integerBreak = function(n) {
         return ans[n];  
     };
     
-    
     const helper = (n) => {
         if (n in ans){
             return ans[n];
         }
-        
         if (n === 2 || n === 3){
             return n-1;
         }
-
         let maxPossible = 1;
         for(let num1 = 1;num1 <= Math.floor(n/2);num1++){
-            const num2 = n - num1;
-            let break1 = 0;
-            let break2 = 0;
-            
-            break1 = (num1 <= 3)? num1:fetchCached(num1);
-            break2 = (num2 <= 3)? num2:fetchCached(num2);
+            let num2 = n - num1;
+            let break1 = (num1 <= 3)? num1:fetchCached(num1);
+            let break2 = (num2 <= 3)? num2:fetchCached(num2);
             maxPossible = Math.max(maxPossible,break1 * break2);
         }
         ans[n] = maxPossible;
