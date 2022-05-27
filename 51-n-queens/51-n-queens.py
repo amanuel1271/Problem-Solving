85d2cd69f7fd0078a230fb42f709fe124b36a966
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-27 23:32:14 +0900

    Time: 75 ms (69.59%), Space: 14.6 MB (15.23%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n):
+        col,posDiag,negDiag = set(),set(),set()
+        ans = []
+        res = [['.' for _ in range(n)] for _ in range(n)]
+        
+        
+        def backtrack(r):
+            if r == n:
+                fin_ans = [''.join(row) for row in res[:]]
+                ans.append(fin_ans)
+                return
+             
+            for c in range(n):
+                if c in col or r+c in negDiag or r-c in posDiag:
+                    continue
+                
+                col.add(c)
+                res[r][c] = 'Q'
+                posDiag.add(r-c)
+                negDiag.add(r+c)
+                
+                backtrack(r+1)
+                
+                col.remove(c)
+                res[r][c] = '.'
+                posDiag.remove(r-c)
+                negDiag.remove(r+c)
+        
+        backtrack(0)
+        return ans
+        
