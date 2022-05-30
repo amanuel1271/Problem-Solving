ce164b2a8c1aa597e1611c6b40bd99095c358b50
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-31 04:06:18 +0900

    Time: 53 ms (31.87%), Space: 13.9 MB (20.87%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -2,7 +2,6 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         ch_freq  = Counter(s)
         cnt_to_char = [ch_freq[ch] for ch in ch_freq]
-        
         def max_odd():
             odd_list = list(filter(lambda x: (x%2 == 1),cnt_to_char))
             if len(odd_list) == 0:
@@ -12,7 +11,6 @@ class Solution:
             rest_sum = sum(odd_list) - maxx - len(odd_list) + 1
             return maxx,rest_sum
             
-
         even_list = list(filter(lambda x: (x%2 == 0),cnt_to_char))
         return sum(max_odd()) + sum(even_list)
         
