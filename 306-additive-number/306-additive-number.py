0d52e03c86dd52c1a33373f9031587d43e0925bc
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-30 19:03:44 +0900

    Time: 52 ms (32.56%), Space: 14 MB (28.17%) - LeetHub

--- 306-additive-number/306-additive-number.py
+++ 306-additive-number/306-additive-number.py
@@ -5,20 +5,17 @@ class Solution:
         def dfs(i,stack,sequence_len):
             if i == len(num):
                 return sequence_len >= 3
-            
             if len(stack) <= 1:
                 if num[i] == '0':
                     stack.append(0)
                     return dfs(i+1,stack,sequence_len + 1)
-                
                 for j in range(i,len(num)):
                     stack.append(int(num[i:j+1]))
                     if dfs(j+1,stack[:],sequence_len + 1):
                         return True
                     stack.pop()
                 return False
-            
-
+        
             top = stack.pop()
             bottom = stack.pop()
             if num[i] == '0':
