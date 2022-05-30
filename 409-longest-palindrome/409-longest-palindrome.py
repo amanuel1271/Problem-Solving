b3dcc4bb952d4a114facbc4141d808cd43dc618d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-31 04:10:58 +0900

    Time: 45 ms (52.64%), Space: 13.8 MB (97.38%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,17 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        ch_freq  = Counter(s)
-        cnt_to_char = [ch_freq[ch] for ch in ch_freq]
-        
-        def max_odd():
-            odd_list = list(filter(lambda x: (x%2 == 1),cnt_to_char))
-            if len(odd_list) == 0:
-                return 0,0
-            
-            maxx = max(odd_list)
-            rest_sum = sum(odd_list) - maxx - len(odd_list) + 1
-            return maxx,rest_sum
-            
-        even_list = list(filter(lambda x: (x%2 == 0),cnt_to_char))
-        return sum(max_odd()) + sum(even_list)
+        ch_freq,cnt,is_there_odd  = Counter(s),0,0
+        for ch in ch_freq:
+            if ch_freq[ch] % 2 == 0:
+                cnt += ch_freq[ch]
+            else:
+                is_there_odd = 1
+                cnt += ch_freq[ch] - 1
+                
+        return cnt + is_there_odd
         
