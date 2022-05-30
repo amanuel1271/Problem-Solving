a96486ae96bf360ac6b0c731e944ea763d8eb081
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-31 05:36:29 +0900

    Time: 2094 ms (19.57%), Space: 14 MB (31.46%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,31 +1,14 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
-        def compare_arrays(arr1,arr2):
-            for i in range(26):
-                if arr1[i] != arr2[i]:
-                    return False 
-            return True
-        
-        s2 = 'z' + s2
-        size1,size2 = len(s1),len(s2)
-        if size1 > size2:
-            return False
-        
-        freq_count,window_array = [0 for _ in range(26)],[0 for _ in range(26)]
-        for ch in s1:
-            freq_count[ord(ch)- ord('a')] += 1
-        for ch in s2[:size1]:
-            window_array[ord(ch)- ord('a')] += 1
-            
-        i = 0
-        while i <= size2-size1-1:
-            window_array[ord(s2[i]) - ord('a')] -= 1
-            i += 1
-            window_array[ord(s2[i + size1-1]) - ord('a')] += 1
-            
-            if compare_arrays(window_array,freq_count):
+        stat = Counter(s1)
+        length1 = len(s1)
+        length2 = len(s2)
+
+        index = 0
+        while index <= length2 - length1:
+            substrStat = Counter(s2[index:index+length1])
+            if substrStat == stat:
                 return True
-            
-        
+            index += 1
+
         return False
