aee58369f9a7d1bcff53e92ce499f31a9863529a
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-05-31 05:45:53 +0900

    Time: 1440 ms (23.88%), Space: 14 MB (31.46%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,17 +1,13 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         stat = Counter(s1)
-        ans = [stat[chr(ord('a') + i)] for i in range(26)]
-        
-        
         length1 = len(s1)
         length2 = len(s2)
 
         index = 0
         while index <= length2 - length1:
             substat = Counter(s2[index:index+length1])
-            subans = [substat[chr(ord('a') + i)] for i in range(26)]
-            if subans == ans:
+            if stat == substat:
                 return True
             index += 1
 
