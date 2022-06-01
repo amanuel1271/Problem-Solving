a45d926e12fe5c0c6619984d9832f4f77558c643
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-02 08:02:24 +0900

    Time: 2402 ms (5.09%), Space: 14.8 MB (93.64%) - LeetHub

--- 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
@@ -2,11 +2,9 @@ class Solution:
     def checkIfCanBreak(self, s1: str, s2: str) -> bool:
         
         def canbreak(s1,s2):
-            freq_count = Counter(s1)
             count = [0]*26
-            
-            for ch in freq_count:
-                count[ord(ch) - ord('a')] = freq_count[ch]
+            for ch in s1:
+                count[ord(ch) - ord('a')] += 1
                 
             for ch in s2:
                 found_bigger = False
