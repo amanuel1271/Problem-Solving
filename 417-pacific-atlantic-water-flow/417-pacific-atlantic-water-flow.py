47e35b197b8685c0b77f960a71adad8a43487768
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-03 11:28:07 +0900

    Time: 447 ms (36.39%), Space: 15.6 MB (77.99%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -6,7 +6,6 @@ class Solution:
         atlantic_set = set([(m-1,c) for c in range(n)] + [(r,n-1) for r in range(m)])
         atlantic_queue = deque(list(atlantic_set))
             
-        
         def bfs(queue,visited):
             while queue:
                 x,y = queue.popleft()
@@ -15,10 +14,10 @@ class Solution:
                         if heights[next_x][next_y] >= heights[x][y]:
                             visited.add((next_x,next_y))
                             queue.append((next_x,next_y))
+                            
         
         bfs(pacific_queue,pacific_set)
         bfs(atlantic_queue,atlantic_set)
-        
         return list(pacific_set.intersection(atlantic_set))
                 
                         
