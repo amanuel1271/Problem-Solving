806acf984efac2ee5820775f2c235bba0ad7e4b6
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-04 00:45:58 +0900

    Time: 65 ms (52.14%), Space: 15.2 MB (79.18%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,14 +1,15 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         info = []
+        freq_ = Counter(s)
         ALPHASIZE,DIGITSIZE = 26,10
         
         for alpha in 'abcdefghijklmnopqrstuvwxyz':
-            info.append((-1 * s.count(alpha),alpha))
-            info.append((-1 * s.count(alpha.upper()),alpha.upper()))
+            info.append((-1 * freq_[alpha],alpha))
+            info.append((-1 * freq_[alpha.upper()],alpha.upper()))
             
         for digit in '0123456789':
-            info.append((-1 * s.count(digit),digit))
+            info.append((-1 * freq_[digit],digit))
         
         heapify(info)
         ans = ''
