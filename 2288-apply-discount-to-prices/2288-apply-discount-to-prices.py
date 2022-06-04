be41afaaf64b8d11a5845c26feaa469ca16c00a9
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-05 03:49:32 +0900

    Time: 144 ms (86.81%), Space: 16 MB (90.70%) - LeetHub

--- 2288-apply-discount-to-prices/2288-apply-discount-to-prices.py
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.py
@@ -4,19 +4,9 @@ class Solution:
         split_s= sentence.split()
         
         for i,word in enumerate(split_s):
-            if word[0] == '$':
-                digit = ''
-                all_digits = True
-                for dig in word[1:]:
-                    if dig.isdigit():
-                        digit += dig
-                    else:
-                        all_digits = False
-                        break
-                        
-                if all_digits and digit != '':
-                    num = int(digit)
-                    split_s[i] = '$' + str(format(num - (num * discount/100),'.2f'))
+            if word[0] == '$' and word[1:].isdigit() and word[1:] != '':
+                num = int(word[1:])
+                split_s[i] = '$' + str(format(num - (num * discount/100),'.2f'))
         
         return ' '.join(split_s)
                     
