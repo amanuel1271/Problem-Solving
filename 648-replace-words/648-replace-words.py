e016b5d5000e75cd602f0ecdb0971f4d1b384766
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-08 09:39:19 +0900

    Time: 148 ms (64.85%), Space: 35.8 MB (37.16%) - LeetHub

---
+++ 648-replace-words/648-replace-words.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.end = False
+        self.children = {}
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self,word):
+        tmp = self.root
+        for ch in word:
+            if ch in tmp.children:
+                tmp = tmp.children[ch]
+            else:
+                tmp.children[ch] = TrieNode()
+                tmp = tmp.children[ch]
+                
+        tmp.end = True
+        
+    def search(self,word):
+        tmp = self.root
+        for i,ch in enumerate(word):
+            if ch in tmp.children:
+                if tmp.children[ch].end:
+                    return word[:i+1]
+                tmp = tmp.children[ch]
+            else:
+                break
+                
+        return word # if no root found return the whole word
+            
+
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
+            
+        sentence_lst = sentence.split()
+        return ' '.join(list(map(lambda word: trie.search(word),sentence_lst)))
+        
