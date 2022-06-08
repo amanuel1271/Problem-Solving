01cdf6e761350b3f9f05c5c0df35fac660a33c9d
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-09 07:13:10 +0900

    Time: 235 ms (66.80%), Space: 14.7 MB (7.16%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.py
+++ 654-maximum-binary-tree/654-maximum-binary-tree.py
@@ -9,14 +9,25 @@ class Solution:
         
         index_map = {nums[i]:i for i in range(len(nums))}
         
+        ## to avoid unnecessary copying
+        def maxIndex(i,j):
+            maxi = i
+            for k in range(i+1,j+1):
+                if nums[k] > nums[maxi]:
+                    maxi = k
+                    
+            return maxi
+            
+        
         def helper(i,j):
             if i > j:
                 return None
             
-            max_index = index_map[(max(nums[i:j+1]))]
+            max_index = maxIndex(i,j)
             root = TreeNode(nums[max_index])
             root.left = helper(i,max_index-1)
             root.right = helper(max_index + 1,j)
             return root
+            
         
         return helper(0,len(nums)-1)
