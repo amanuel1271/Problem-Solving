3604d3b07a7f9a1da0e602609c3647c29a9ad4b6
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-15 06:43:22 +0900

    Time: 1887 ms (13.62%), Space: 210.1 MB (6.27%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.py
@@ -11,8 +11,7 @@ class Solution:
             if s1[i] == s2[j]:
                 return delete(i+1,j+1)
             
-            return min(delete(i+1,j) + ord(s1[i]),delete(i,j+1) + ord(s2[j]),delete(i+1,j+1) + ord(s1[i]) + ord(s2[j]))
-            
+            return min(delete(i+1,j) + ord(s1[i]),delete(i,j+1) + ord(s2[j]))
             
         return delete(0,0)
         
