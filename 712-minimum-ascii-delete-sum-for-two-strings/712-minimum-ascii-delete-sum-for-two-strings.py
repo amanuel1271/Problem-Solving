1aa0a3bebb99204c1c98566788965168f06a4c92
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-15 07:00:29 +0900

    Time: 932 ms (63.41%), Space: 18.5 MB (62.55%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,17 +1,19 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        row,col = len(s1),len(s2)
+        dp = [[0]*(col+1) for _ in range(row+1)]
         
-        @lru_cache(None)
-        def delete(i,j):
-            if i == len(s1):
-                return sum([ord(ch) for ch in s2[j:]])
-            elif j == len(s2):
-                return sum([ord(ch) for ch in s1[i:]])
+        for i in range(row):
+            dp[i][col] = sum([ord(ch) for ch in s1[i:]])
+        for j in range(col):
+            dp[row][j] = sum([ord(ch) for ch in s2[j:]])
             
-            if s1[i] == s2[j]:
-                return delete(i+1,j+1)
+        for i in range(row-1,-1,-1):
+            for j in range(col-1,-1,-1):
+                if s1[i] == s2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = min(dp[i+1][j] + ord(s1[i]),dp[i][j+1] + ord(s2[j]))
             
-            return min(delete(i+1,j) + ord(s1[i]),delete(i,j+1) + ord(s2[j]))
-            
-        return delete(0,0)
+        return dp[0][0]
         
