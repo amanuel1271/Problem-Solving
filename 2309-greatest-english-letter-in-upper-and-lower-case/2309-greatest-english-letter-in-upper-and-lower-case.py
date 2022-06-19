751b8dc578d349b272d3710da674ba53c6bc8b51
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-19 15:31:40 +0900

    Time: 109 ms (20.00%), Space: 13.9 MB (20.00%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -0,0 +1,14 @@
+class Solution:
+    def greatestLetter(self, s: str) -> str:
+        maxx = 0
+        ch_map = dict()
+        
+        
+        for ch in s:
+            ch_map[ch] = True
+            
+            if ch.lower() in ch_map and ch.upper() in ch_map:
+                maxx = max(maxx,ord(ch.upper()))
+                
+                
+        return '' if maxx == 0 else chr(maxx)
