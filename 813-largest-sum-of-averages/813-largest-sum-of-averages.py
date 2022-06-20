9dfbc11677f5c11cd542f41c73a65511570a9941
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-20 13:35:41 +0900

    Time: 294 ms (84.47%), Space: 15.3 MB (29.97%) - LeetHub

---
+++ 813-largest-sum-of-averages/813-largest-sum-of-averages.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
+        
+        @lru_cache(None)
+        def search(n, k):
+            if n < k: return 0
+            if k == 1:
+                return  sum(nums[:n]) / float(n)
+            
+            cur,res = 0,0
+            for i in range(n - 1, 0, -1):
+                cur += nums[i]
+                res = max(res, search(i, k - 1) + cur / float(n - i))
+            return res
+        
+        return search(len(nums), k)
