1d929bacf89dcf28f51503df6e5055f0cf669b2b
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-20 13:41:44 +0900

    Time: 334 ms (78.20%), Space: 15.3 MB (31.88%) - LeetHub

--- 813-largest-sum-of-averages/813-largest-sum-of-averages.py
+++ 813-largest-sum-of-averages/813-largest-sum-of-averages.py
@@ -10,7 +10,7 @@ class Solution:
             cur,res = 0,0
             for i in range(n - 1, -1, -1):
                 cur += nums[i]
-                res = max(res, search(i, k - 1) + cur / float(n - i))
+                res = max(res, search(i, k - 1) + (cur / float(n - i)))
             return res
         
         return search(len(nums), k)
