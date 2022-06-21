521df839da56a1bd4919ee06a4ab28fe23232d95
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-21 12:07:56 +0900

    Time: 146 ms (84.14%), Space: 14.7 MB (32.45%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.py
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.py
@@ -10,10 +10,9 @@ class Solution:
             
         res = max(i,len(seats)-1-m)
         
-        is_zero,zeroes = False,0
+        zeroes = 0
         for i in range(len(seats)):
             if seats[i] == 1:
-                is_zero = False
                 res = max(res,math.ceil(zeroes/2))
                 zeroes = 0
             else:
