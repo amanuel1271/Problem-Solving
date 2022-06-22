ebf75f186d9e9002586ffd7b14edfcf1257c07cb
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-23 01:46:57 +0900

    Time: 8887 ms (5.01%), Space: 86.2 MB (5.00%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.py
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         dp = {}
-        max_ = 0
+        maxx = 0
         
         for i in range(1,len(arr)):
             for j in range(i):
                 if arr[i]-arr[j] < arr[j] and (arr[i]-arr[j],arr[j]) in dp:
                     dp[(arr[j],arr[i])] = dp[(arr[i]-arr[j],arr[j])] + 1
-                    max_ = max(max_,dp[(arr[j],arr[i])])
+                    maxx = max(maxx,dp[(arr[j],arr[i])])
                 else:
                     dp[(arr[j],arr[i])] = 2
                     
-        return max_ if max_ >= 3 else 0
+        return maxx if maxx >= 3 else 0
