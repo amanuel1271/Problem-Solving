e423a63f234ddaf063457a26856727deb6b06f82
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-23 00:49:53 +0900

    Time: 78 ms (5.36%), Space: 14.1 MB (53.01%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.py
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -7,20 +7,24 @@
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
         depth = {root: 0,None: -1}
+        self.maxdepth = 0
+        
         def dfs(node,parent):
             if node:
                 depth[node] = depth[parent] + 1
+                self.maxdepth = max(self.maxdepth,depth[node])
                 dfs(node.left, node)
                 dfs(node.right, node)
                 
         dfs(root.left,root)
         dfs(root.right,root)
 
-        max_depth = max(depth.values())
-
         def answer(node):
-            if not node or depth[node] == max_depth:
+            if not node:
+                return None
+            elif depth[node] == self.maxdepth:
                 return node
+            
             L, R = answer(node.left), answer(node.right)
             return node if L and R else L or R
 
