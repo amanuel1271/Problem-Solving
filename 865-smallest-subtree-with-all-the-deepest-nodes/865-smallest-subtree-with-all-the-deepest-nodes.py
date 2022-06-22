70fb717d9e92049fc627d0af2908fefeab5fa746
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-23 00:27:20 +0900

    Time: 85 ms (5.36%), Space: 14.1 MB (53.01%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.py
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -6,52 +6,27 @@
 #         self.right = right
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-        if not root or (not root.left and not root.right):
-            return root
-        
-        node_to_par = dict()
-        val_to_node = dict()
-        Q = deque([root])
-        last_level_nodes = []
-        
-        while Q:
-            size = len(Q)
-            save,last_level_nodes,last_level = last_level_nodes,[],True
-            for i in range(size):
-                node = Q.popleft()
-                val_to_node[node.val] = node
-                if node.left:
-                    last_level = False
-                    node_to_par[node.left] = node
-                    Q.append(node.left)
-                    last_level_nodes.append(node.left.val)
-                if node.right:
-                    last_level = False
-                    node_to_par[node.right] = node
-                    Q.append(node.right)
-                    last_level_nodes.append(node.right.val)
-                    
-            if last_level:
-                last_level_nodes = save
+        depth = {root: 0}
+        def dfs(node,parent):
+            if node:
+                depth[node] = depth[parent] + 1
+                dfs(node.left, node)
+                dfs(node.right, node)
                 
-        last_level_nodes = deque(last_level_nodes)
-        size = len(last_level_nodes)
+        dfs(root.left,root)
+        dfs(root.right,root)
+
+        max_depth = max(depth.values())
+
+        def answer(node):
+            # Return the answer for the subtree at node.
+            if not node or depth.get(node, None) == max_depth:
+                return node
+            L, R = answer(node.left), answer(node.right)
+            return node if L and R else L or R
+
+        return answer(root)
         
-        while size > 1:
-            visited = set()
-            
-            for i in range(size):
-                val = last_level_nodes.popleft()
-                parent_node = node_to_par[val_to_node[val]]
-                parent_val = parent_node.val
-                visited.add(parent_val)
-                
-            last_level_nodes = deque(list(visited))
-            size = len(last_level_nodes)
-            
-        #print(last_level_nodes)
-        return val_to_node[last_level_nodes.pop()]
-            
                 
                 
         
