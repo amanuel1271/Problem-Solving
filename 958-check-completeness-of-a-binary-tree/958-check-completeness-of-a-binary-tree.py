130075b370b7a247bc74ef107b2fcd378957173b
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-28 06:05:38 +0900

    Time: 58 ms (40.62%), Space: 14 MB (27.44%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.py
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.py
@@ -7,38 +7,26 @@
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
         queue = deque([root])
-        level = 0
         full_level = True
+        is_empty = False
         
         while queue:
             size = len(queue)
-            if size != 2**level:
-                full_level = False
-                
-            is_empty = False
-            num_of_children = 0
-                
             for i in range(size):
                 node = queue.popleft()
                 if node.left:
                     if is_empty:
                         return False
                     queue.append(node.left)
-                    num_of_children += 1
                 else:
                     is_empty = True
                 if node.right:
                     if is_empty:
                         return False
                     queue.append(node.right)
-                    num_of_children += 1
                 else:
                     is_empty = True
                     
-            level += 1
-                    
-            if not full_level and num_of_children > 0:
-                return False
             
         return True
                 
