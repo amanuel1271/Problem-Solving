10c01ea216e84c3f08ccd5df2909cf06adc6b0fc
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-06-30 14:15:10 +0900

    Time: 2030 ms (17.21%), Space: 15 MB (75.05%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -3,15 +3,9 @@ class Solution:
         
         def moves_to_make_equi(arr1,arr2):
             count_freq = Counter(arr1)
-            num_with_max_freq = []
-            max_freq = max(count_freq.values())
-            
-            for num in range(1,7):
-                if count_freq[num] == max_freq:
-                    num_with_max_freq.append(num)
             
             min_swaps = math.inf
-            for num in num_with_max_freq:
+            for num in range(1,7):
                 is_possible = True
                 swap_cnt = 0
                 for i in range(len(arr1)):
