d0eb604756bf79a7d3b23a71f2b318a95de062ab
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-07-02 15:11:18 +0900

    Time: 61 ms (81.45%), Space: 14.2 MB (25.63%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,38 +1,40 @@
 class Solution:
     def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:\
         
-        def second_arr(arr):
-            l,r = 0,secondLen-1
-            window_sum = sum(arr[l:r+1])
-            maxx = window_sum
-            r += 1
+        def slide(r_len):
+            l_len = firstLen + secondLen - r_len
             
-            while r < len(arr):
-                window_sum += arr[r]
-                window_sum -= arr[l]
-                l += 1
-                maxx = max(maxx,window_sum)
-                r += 1
-            return maxx
-        
-        def firstarr():
-            l,r = 0,firstLen-1
-            window_sum = sum(nums[l:r+1])
-            maxx = window_sum + second_arr(nums[:l] + nums[r+1:])
-            r += 1
+            # boundary of the left window and right window
+            r_l,r_r = len(nums)-r_len,len(nums)-1
+            l_r = r_l-1
+            l_l = l_r - (l_len - 1)
             
-            while r < len(nums):
-                window_sum += nums[r]
-                window_sum -= nums[l]
-                l += 1
+            left_window_sum,right_window_sum = sum(nums[l_l : l_r+1]),sum(nums[r_l:r_r+1])
+            r_max = right_window_sum
+            max_val = left_window_sum + r_max
+            l_l -= 1
+            r_l -= 1
+            
+            
+            while l_l >= 0:
+                left_window_sum += nums[l_l]
+                left_window_sum -= nums[l_r]
+                l_r -= 1
+                right_window_sum += nums[r_l]
+                right_window_sum -= nums[r_r]
+                r_r -= 1
                 
-                cand = window_sum + second_arr(nums[:l] + nums[r+1:])
-                maxx = max(maxx,cand)
-                r += 1
+                r_max = max(r_max,right_window_sum)
+                max_val = max(max_val, r_max + left_window_sum)
+                l_l -= 1
+                r_l -= 1
                 
-            return maxx
-        
-        return firstarr()
+            return max_val
+                
+                
+                
+
+        return max(slide(firstLen),slide(secondLen))
                 
             
             
