5de323981b625807d405b0729f3affc5d099f865
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-07-18 17:29:49 +0900

    Update Sparse_matrix_mult.py

--- Sparse_matrix_mult.py
+++ Sparse_matrix_mult.py
@@ -1,20 +1,28 @@
 def sparse_matrix_multiplication(matrix_a, matrix_b):
     # Write your code here.
-    if matrix_a == [] or matrix_b == []:
+    
+    def can_multiply():
+        if matrix_a == [] or matrix_b == []:
+            return False
+    
+        m = len(matrix_a[0])
+        for row in matrix_a[1:]:
+            if len(row) != m:
+                return False
+                
+        n = len(matrix_b[0])
+        for row in matrix_b[1:]:
+            if len(row) != n:
+                return False
+        if len(matrix_b) != m:
+            return False
+        return True
+        
+        
+    if not can_multiply():
         return [[]]
-
-    m = len(matrix_a[0])
-    for row in matrix_a[1:]:
-        if len(row) != m:
-            return [[]]
-            
-    n = len(matrix_b[0])
-    for row in matrix_b[1:]:
-        if len(row) != n:
-            return [[]]
-    if len(matrix_b) != m:
-        return [[]]
-
+    
+    n,m = len(matrix_b[0]),len(matrix_a[0])
     non_zero_row = {i:set() for i in range(len(matrix_a))}
     non_zero_col = {i:set() for i in range(n)}
 
