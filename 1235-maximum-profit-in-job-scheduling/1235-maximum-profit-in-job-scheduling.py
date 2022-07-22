98f54717d1cd466a657c4d9bbbea91db096b4b57
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-07-22 14:33:45 +0900

    Time: 1185 ms (20.47%), Space: 110.3 MB (5.14%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        n = len(startTime)
+        jobs = sorted(list(zip(startTime, endTime, profit)))
+        startTime = [jobs[i][0] for i in range(n)]
+
+        @lru_cache(None)
+        def dp(i):
+            if i == n: return 0
+            ans = dp(i + 1)  # Choice 1: Don't pick
+
+            j = bisect_left(startTime, jobs[i][1])
+            ans = max(ans, dp(j) + jobs[i][2])  # Choice 2: Pick
+            return ans
+
+        return dp(0)
+        
