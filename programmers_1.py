9cb39706ca4fccfd8e42f15b21492883b784207f
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-07-27 04:44:59 +0900

    Update programmers_1.py

--- programmers_1.py
+++ programmers_1.py
@@ -1,26 +1,24 @@
 #programmers.co.kr practice 
-
-
 import math
-
 answer = math.inf
 
-def DFS(n, pos, num, number):
-    global answer
-    if pos > 8:
-        return
-    if num == number:
-        answer = min(answer,pos)
-        return
 
-    nn=0
-    for i in range(8):
-        nn=nn*10+n
-        DFS(n, pos+1+i, num+nn, number)
-        DFS(n, pos+1+i, num-nn, number)
-        DFS(n, pos+1+i, num*nn, number)
-        DFS(n, pos+1+i, num/nn, number)
+def solution(N, number):   
+    def DFS(pos, num, number):
+        global answer
+        if pos > 8:
+            return
+        if num == number:
+            answer = min(answer,pos)
+            return
 
-def solution(N, number):    
-    DFS(N, 0, 0, number)    
+        nn=0
+        for i in range(8):
+            nn=nn*10+N
+            DFS(pos+1+i, num+nn, number)
+            DFS(pos+1+i, num-nn, number)
+            DFS(pos+1+i, num*nn, number)
+            DFS(pos+1+i, num/nn, number)
+            
+    DFS(0, 0, number)    
     return answer if answer != math.inf else -1
