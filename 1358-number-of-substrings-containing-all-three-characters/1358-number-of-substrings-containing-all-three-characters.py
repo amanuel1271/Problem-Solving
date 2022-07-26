32a946e38f5158f17444841878b6d08355454add
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-07-27 00:13:34 +0900

    Time: 539 ms (19.12%), Space: 14.5 MB (34.18%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -5,7 +5,7 @@ class Solution:
         
         for r in range(len(s)):
             freq_cnt[s[r]] += 1
-            while l <= r and freq_cnt['a'] > 0 and freq_cnt['b'] > 0 and freq_cnt['c'] > 0:
+            while freq_cnt['a'] > 0 and freq_cnt['b'] > 0 and freq_cnt['c'] > 0:
                 cnt += len(s)-r
                 freq_cnt[s[l]] -= 1
                 l += 1
