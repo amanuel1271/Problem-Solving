63591139c10a0b0d6601add72b8b712cc444a895
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-09-04 04:03:55 +0900

    Update Lily's Homework.py

--- Lily's Homework.py
+++ Lily's Homework.py
@@ -17,12 +17,26 @@ def swap_all(dic,arr,i,index_sorted):
     dic[arr[i]],dic[arr[index_sorted]] = index_sorted,i
     arr[i],arr[index_sorted] = arr[index_sorted],arr[i]
     
+    
+    
+    
+    
+def swap_cnt(asc_num_to_index,asc_sorted,cmp_asc):
+    swap = 0
+    
+    for i in range(len(cmp_asc)):
+        if cmp_asc[i] == asc_sorted[i]:
+            continue
+        index_sorted = asc_num_to_index[asc_sorted[i]]
+        swap_all(asc_num_to_index,cmp_asc,i,index_sorted)
+        swap += 1
+    return swap
+    
 
 def lilysHomework(arr):
     asc_sorted = sorted(arr)
     desc_sorted = asc_sorted[::-1]
     cmp_asc,cmp_desc = arr[:],arr[:]
-    
     asc_num_to_index,asc_swap = {},0
     desc_num_to_index,desc_swap = {},0
     
@@ -30,24 +44,7 @@ def lilysHomework(arr):
         asc_num_to_index[num] = i
         desc_num_to_index[num] = i
     
-    #ascending
-    for i in range(len(cmp_asc)):
-        if cmp_asc[i] == asc_sorted[i]:
-            continue
-        
-        index_sorted = asc_num_to_index[asc_sorted[i]]
-        swap_all(asc_num_to_index,cmp_asc,i,index_sorted)
-        asc_swap += 1
-        
-    #descending
-    for i in range(len(cmp_desc)):
-        if cmp_desc[i] == desc_sorted[i]:
-            continue
-        index_sorted = desc_num_to_index[desc_sorted[i]]
-        swap_all(desc_num_to_index,cmp_desc,i,index_sorted)
-        desc_swap += 1
-    
-    return min(asc_swap,desc_swap)
+    return min(swap_cnt(asc_num_to_index,asc_sorted,cmp_asc), swap_cnt(desc_num_to_index,desc_sorted,cmp_desc))
         
 
 if __name__ == '__main__':
