fa05f39a59b9ad3c8ba48f048d6012efd859efa7
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-09-16 08:30:20 +0900

    Time: 1484 ms (35.52%), Space: 32.3 MB (28.96%) - LeetHub

--- 1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.py
+++ 1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.py
@@ -1,20 +1,15 @@
 class Solution:
     def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
-        index_to_set,ans = [],[]
+        to_set,ans = [],[]
         for i in range(len(favoriteCompanies)):
-            index_to_set.append(set(favoriteCompanies[i]))
+            to_set.append(set(favoriteCompanies[i]))
             
-        for i in range(len(favoriteCompanies)):
-            set_size = len(index_to_set[i])
-            is_subset = False
-            for j in range(len(favoriteCompanies)):
-                if i == j:
-                    continue
-                if set_size <= len(index_to_set[j]) and set_size == len(index_to_set[i].intersection(index_to_set[j])):
-                    is_subset = True
-                    break
-            if not is_subset:
-                ans.append(i)
+        for i,s1 in enumerate(to_set):
+            set_size = len(s1)
+            if any(i != j and (set_size <= len(s2) and len(s1.intersection(s2)) == set_size) for j,s2 in enumerate(to_set)):
+                continue
+                
+            ans.append(i)
                 
         return ans
                 
