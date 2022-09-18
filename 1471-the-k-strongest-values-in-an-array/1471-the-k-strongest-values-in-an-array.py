6f08ccee14c7519fc6d2106dd52c04e81e14a70c
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2022-09-18 23:12:51 +0900

    Time: 2496 ms (13.20%), Space: 27.6 MB (81.22%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.py
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.py
@@ -5,13 +5,14 @@ class Solution:
         arr.sort()
         median = arr[(len(arr)-1)//2]
         
-        while l <= r and len(ans) < k:
+        while k > 0:
             if abs(arr[r] - median) >= abs(arr[l]-median):
                 ans.append(arr[r])
                 r -= 1
             else:
                 ans.append(arr[l])
                 l += 1
+            k -= 1
                 
         return ans
                 
